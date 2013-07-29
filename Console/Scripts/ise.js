387ef00e2b5650e948db32ea609e6fb8d2c9ff1f
Michael West <michaellwest@gmail.com>
2013-07-28 20:44:11 -0500

    Minified extend function.

--- Console/Scripts/ise.js
+++ Console/Scripts/ise.js
@@ -5,23 +5,7 @@ function onExecute() {
 
 // a convenience function for parsing string namespaces and
 // automatically generating nested namespaces
-function extend(ns, ns_string) {
-    var parts = ns_string.split('.'),
-        parent = ns,
-        pl, i;
-    if (parts[0] == "cognifide") {
-        parts = parts.slice(1);
-    }
-    pl = parts.length;
-    for (i = 0; i < pl; i++) {
-        //create a property if it doesnt exist
-        if (typeof parent[parts[i]] == 'undefined') {
-            parent[parts[i]] = {};
-        }
-        parent = parent[parts[i]];
-    }
-    return parent;
-}
+function extend(e, t) { var n = t.split("."), r = e, i, s; if (n[0] == "cognifide") { n = n.slice(1) } i = n.length; for (s = 0; s < i; s++) { if (typeof r[n[s]] == "undefined") { r[n[s]] = {} } r = r[n[s]] } return r }
 
 var cognifide = cognifide || {};
 extend(cognifide, 'powershell');
