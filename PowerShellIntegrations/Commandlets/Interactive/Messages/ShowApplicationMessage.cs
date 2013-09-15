9f09d9ff6af7db2b0d45426b46a3bffb499fc9cf
Adam Najmanowicz <adam@najmanowicz.com>
2013-09-15 13:06:20 +0200

    Issue #96

---
+++ PowerShellIntegrations/Commandlets/Interactive/Messages/ShowApplicationMessage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cognifide.PowerShell.PowerShellIntegrations.Host;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Express;
+using Sitecore.Jobs.AsyncUI;
+using Sitecore.SecurityModel;
+using Sitecore.Shell.Framework;
+using Sitecore.Text;
+using Sitecore.Threading;
+using Sitecore.Web.UI.Sheer;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Interactive.Messages
+{
+    public class ShowApplicationMessage : IMessage
+    {
+        public List<ShowListViewCommand.SvlDataObject> Data { get; set; }
+        public Hashtable Parameters { get; set; }
+        public string AppName { get; private set; }
+        public string Title { get; set; }
+        public string Width { get; private set; }
+        public string Height { get; private set; }
+        public string Icon { get; private set; }
+        public bool Modal { get; set; }
+
+
+        public ShowApplicationMessage(string application, string title, string icon, string width, string height, bool modal, Hashtable parameters)
+        {
+            Title = title;
+            Width = width;
+            Height = height;
+            Icon = icon;
+            Modal = modal;
+            Parameters = parameters;
+            AppName = application;
+        }
+
+
+        public void Execute()
+        {
+            if (!Modal)
+            {
+                UrlString urlString = new UrlString();
+                AddParameters(urlString);
+                Item appItem =
+                    Database.GetDatabase("core").GetItem("/sitecore/content/Applications/"+AppName);
+                Windows.RunApplication(appItem, Icon ?? appItem["Icon"], Title ?? appItem["Display name"], urlString.Query);
+            }
+            else
+            {
+                UrlString urlString = new UrlString("/sitecore/shell/sitecore/content/Applications/"+AppName+".aspx");
+                AddParameters(urlString);
+                SheerResponse.ShowModalDialog(urlString.ToString(), Width, Height);
+            }
+        }
+
+        private void AddParameters(UrlString urlString)
+        {
+            if (Parameters != null)
+            {
+                foreach (string key in Parameters.Keys)
+                {
+                    urlString.Add(key, Parameters[key].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ PowerShellIntegrations/Commandlets/Interactive/ShowApplicationCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Management.Automation;
+using Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Interactive.Messages;
+using Sitecore.Jobs.AsyncUI;
+using Sitecore.Web;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Interactive
+{
+    [Cmdlet(VerbsCommon.Show, "Application")]
+    public class ShowApplicationCommand : BaseFormCommand
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Application { get; set; }
+
+/*
+        [Parameter]
+        public string Url { get; set; }
+*/
+        [Parameter(Position = 1)]
+        public Hashtable Parameter{ get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public SwitchParameter Modal { get; set; }
+
+        [Parameter(ValueFromPipeline = true)]
+        public object Data { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            LogErrors(() =>
+                {
+                        var message =
+                            new ShowApplicationMessage(Application, Title, Icon, WidthString, HeightString, Modal.IsPresent, Parameter);
+
+                        JobContext.MessageQueue.PutMessage(message);
+                        JobContext.Flush();
+                });
+        }
+    }
+}
