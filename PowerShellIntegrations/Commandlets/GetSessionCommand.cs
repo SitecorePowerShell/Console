00ab0c279b1dabd1e3da3a158462dd70950116d3
Adam Najmanowicz <adam@najmanowicz.com>
2013-09-19 17:11:43 +0200

    Issue #103

---
+++ PowerShellIntegrations/Commandlets/GetSessionCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Web;
+using Sitecore.Data.Indexing;
+using Sitecore.Web.Authentication;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets
+{
+    [Cmdlet("Get", "Session")]
+    [OutputType(new[] { typeof(Index) })]
+    public class GetSessionCommand : BaseCommand
+    {
+        [Parameter]
+        public string User { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            WildcardWrite(User, DomainAccessGuard.Sessions, s=> s.UserName);
+        }
+    }
+}
