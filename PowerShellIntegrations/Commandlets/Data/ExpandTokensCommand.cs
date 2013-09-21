86ecc4a8ae40590fb5c6d70fde8e49bc9eb865e2
Mike Reynolds <sitecore.junkie@gmail.com>
2013-09-20 21:59:26 -0400

    Added a new command that expands tokens on a supplied item
    This command uses an instance of the MasterVariablesReplacer for the
    given Sitecore solution as defined in the instance's Web.config to
    replace tokens on a supplied item.

---
+++ PowerShellIntegrations/Commandlets/Data/ExpandTokensCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management.Automation;
+
+using Sitecore.Configuration;
+
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Data
+{
+    [Cmdlet("Expand", "Tokens", DefaultParameterSetName = "Item")]
+    [OutputType(new[] { typeof(Item) })]
+    public class ExpandTokensCommand : BaseCommand
+    {
+        private static readonly MasterVariablesReplacer TokenReplacer = Factory.GetMasterVariablesReplacer();
+
+        [Parameter(Position = 0, Mandatory = true)]
+        public Item Item { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Item.Editing.BeginEdit();
+            try
+            {
+                TokenReplacer.ReplaceItem(Item);
+                Item.Editing.EndEdit();
+            }
+            catch (Exception ex)
+            {
+                Item.Editing.CancelEdit();
+                throw ex;
+            }
+
+            PSObject psobj = ItemShellExtensions.GetPsObject(SessionState, Item);
+            WriteObject(psobj, false);
+        }
+    }
+}
