9ed6abec62d1861a48e3176c19c8ae1ebb598b9e
Adam Najmanowicz <adam@najmanowicz.com>
2014-07-13 20:50:54 +0200

    Issue #229 - should deal with script relative paths.

--- Documentation/GenerateHelp.ps1
+++ Documentation/GenerateHelp.ps1
@@ -1,12 +1,13 @@
-﻿#Trace-Command -Name ParameterBinding -PSHost -Expression { "michael" | Get-Session }
-$currentDirectory = "C:\inetpub\wwwroot\Console72"
-$documentation = Join-Path -Path $currentDirectory -ChildPath "Documentation"
-$help = Join-Path -Path $currentDirectory -ChildPath "Console\Assets"
-$moduleLibraryPath = (Join-Path -Path $currentDirectory -ChildPath "Website\bin\Cognifide.PowerShell.dll")
+﻿$Invocation = Resolve-Path ($MyInvocation.MyCommand.Definition)
+$documentation = Split-Path $Invocation
+$rootDirectory = Split-Path $documentation -Parent
+
+$help = Join-Path -Path $rootDirectory -ChildPath "Console\Assets"
+$moduleLibraryPath = (Join-Path -Path $rootDirectory -ChildPath "bin\Debug\Cognifide.PowerShell.dll")
 if(!(Test-Path -Path $moduleLibraryPath)) {
     Write-Error "Module Library Path not found"
 }
-$helpLibraryPath = (Join-Path -Path $currentDirectory -ChildPath "Libraries\PowerShell.MamlGenerator.dll")
+$helpLibraryPath = (Join-Path -Path $rootDirectory -ChildPath "Libraries\PowerShell.MamlGenerator.dll")
 if(!(Test-Path -Path $helpLibraryPath)) {
     Write-Error "Help Library Path not found"
 }
