6ba5d3cee4cea8f788ff0d53fb85cc5b7cbf7e97
Adam Najmanowicz <adam@najmanowicz.com>
2014-07-18 00:47:48 +0200

    Issue #230

---
+++ PowerShellIntegrations/Commandlets/Data/GetItemFieldCommand.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.Data.Templates;
+using Sitecore.Globalization;
+using Sitecore.Publishing;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Data
+{
+    [Cmdlet(VerbsCommon.Get, "ItemField")]
+    [OutputType(new[] {typeof (Item)}, ParameterSetName = new[] {"Item from Pipeline", "Item from Path", "Item from ID"})]
+    public class GetItemFieldCommand : BaseItemCommand
+    {
+        public enum ReturnValue
+        {
+            Name,
+            Field,
+            TemplateField,
+            TemplateFieldItem
+        }
+
+        [Parameter]
+        public SwitchParameter NoStandardFields { get; set; }
+
+        [Parameter]
+        public ReturnValue ReturnType { get; set; }
+
+        [Parameter]
+        public string[] Name { get; set; }
+
+        protected List<WildcardPattern> NameWildcardPatterns { get; private set; }
+
+        protected override void BeginProcessing()
+        {
+            Language = null;
+            base.BeginProcessing();
+
+            if (Name != null && Name.Any())
+            {
+                NameWildcardPatterns =
+                    Name.Select(
+                        name =>
+                            new WildcardPattern(name, WildcardOptions.IgnoreCase | WildcardOptions.CultureInvariant))
+                        .ToList();
+            }
+        }
+
+        protected override void ProcessItem(Item item)
+        {
+            item.Fields.ReadAll();
+            Template template =
+                TemplateManager.GetTemplate(Sitecore.Configuration.Settings.DefaultBaseTemplate,
+                    item.Database);
+            foreach (Field field in item.Fields)
+            {
+                if (NameWildcardPatterns != null)
+                {
+                    bool wildcardMatch = false;
+                    foreach (var nameWildcardPattern in NameWildcardPatterns)
+                    {
+                        if (nameWildcardPattern.IsMatch(field.Name))
+                        {
+                            wildcardMatch = true;
+                            break;
+                        }
+                    }
+                    if (!wildcardMatch)
+                    {
+                        continue;
+                    }
+
+                }
+                if (NoStandardFields && template.ContainsField(field.ID))
+                {
+                    continue;
+                }
+
+                switch (ReturnType)
+                {
+                    case (ReturnValue.Field):
+                        WriteObject(field);
+                        break;
+                    case (ReturnValue.TemplateField):
+                        WriteObject(field.GetTemplateField());
+                        break;
+                    case (ReturnValue.TemplateFieldItem):
+                        WriteItem(field.Database.GetItem(field.ID));
+                        break;
+                    default:
+                        WriteObject(field.Name);
+                        break;
+                }
+            }
+        }
+    }
+}
