1be9335ecf47bc03a3d3ae71ce59c33cf5e28fcd
Adam Najmanowicz <adam@najmanowicz.com>
2014-07-19 18:05:04 +0200

    Added documentation for Remove-ItemLanguage, related to Issue #184

---
+++ Documentation/Remove-ItemLanguage.ps1
@@ -0,0 +1,49 @@
+<#
+    .SYNOPSIS
+        Removes Language from a single item or a branch of items
+
+    .DESCRIPTION
+        Removes Language version from a an Item either sent from pipeline or defined with Path or ID. A single language or a list of languages can be defined using the Language parameter. 
+	Language  parameter supports globbing so you can delete whole language groups using wildcards.
+
+
+    .PARAMETER Recurse
+        Deleted language versions from the item and all of its children.
+
+    .PARAMETER Language
+        Language(s) that should be deleted form the provided item(s).
+        A single language or a list of languages can be defined using the parameter. 
+	Language parameter supports globbing so you can delete whole language groups using wildcards.
+
+    .PARAMETER Item
+        The item to be processed.
+
+    .PARAMETER Path
+        Path to the item to be processed - can work with Language parameter to narrow the publication scope.
+
+    .PARAMETER Id
+        Id of the item to be processed - can work with Language parameter to narrow the publication scope.
+
+    .PARAMETER Database
+        Database containing the item to be processed - can work with Language parameter to narrow the publication scope.    
+    
+    .INPUTS
+        Sitecore.Data.Items.Item
+    
+    .OUTPUTS
+
+    .NOTES
+        Help Author: Adam Najmanowicz, Michael West
+
+    .LINK
+        https://github.com/SitecorePowerShell/Console/
+
+    .EXAMPLE
+        # Remove Polish and Spanish language from /sitecore/content/home item in the master database
+        PS master:\> Remove-ItemLanguage -Path master:\content\home -Language "pl-pl", "es-es"
+
+    .EXAMPLE
+        # Remove all english based languages defined in /sitecore/content/home item and all of its children in the master database
+        PS master:\> Remove-ItemLanguage -Path master:\content\home -Language "en-*" -Recurse
+
+#>
