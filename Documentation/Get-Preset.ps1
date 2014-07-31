a8fbced7dba67671f1b40d77eaf46f8da31d6b2c
Adam Najmanowicz <adam@najmanowicz.com>
2014-07-31 12:00:04 +0200

    Get-Preset commandlet improved + documentation delivered.

---
+++ Documentation/Get-Preset.ps1
@@ -0,0 +1,50 @@
+<#
+    .SYNOPSIS
+        Returns a serialization preset for use with Serialize-Item.
+
+    .DESCRIPTION
+        Returns a serialization preset for use with Serialize-Item.
+
+
+    .PARAMETER Name
+        Name of the serialization preset.
+    
+    .INPUTS
+    
+    .OUTPUTS
+        Sitecore.Data.Serialization.Presets.IncludeEntry
+
+    .NOTES
+        Help Author: Adam Najmanowicz, Michael West
+
+    .LINK
+        Serialize-Item
+
+    .LINK
+        Deserialize-Item
+
+    .LINK
+        https://github.com/SitecorePowerShell/Console/
+
+    .EXAMPLE
+        PS master:\> Get-Preset -Name "PowerShell", "AssetsOptimiser" | ft PresetName, Database, Path -AutoSize
+        
+        PresetName      Database Path
+        ----------      -------- ----
+        PowerShell      core     /sitecore/templates/Modules/PowerShell Console
+        PowerShell      core     /sitecore/system/Modules/PowerShell/Console Colors
+        PowerShell      core     /sitecore/system/Modules/PowerShell/Script Library
+        PowerShell      core     /sitecore/layout/Layouts/Applications/PowerShell Console
+        PowerShell      core     /sitecore/layout/Layouts/Applications/PowerShell ISE Sheer
+        PowerShell      core     /sitecore/layout/Layouts/Applications/PowerShell ISE
+        PowerShell      core     /sitecore/layout/Layouts/Applications/PowerShell ListView
+        PowerShell      core     /sitecore/content/Documents and Settings/All users/Start menu/Right/PowerShell Toolbox
+        PowerShell      core     /sitecore/content/Applications/PowerShell
+        PowerShell      core     /sitecore/content/Applications/Content Editor/Context Menues/Default/Context PowerShell Scripts
+        PowerShell      master   /sitecore/templates/Modules/PowerShell Console
+        PowerShell      master   /sitecore/system/Modules/PowerShell/Console Colors
+        PowerShell      master   /sitecore/system/Modules/PowerShell/Rules
+        PowerShell      master   /sitecore/system/Modules/PowerShell/Script Library
+        AssetsOptimiser master   /sitecore/templates/Cognifide/Optimiser
+        AssetsOptimiser master   /sitecore/system/Modules/Optimiser
+#>
--- PowerShellIntegrations/Commandlets/Serialization/GetPresetCommand.cs
+++ PowerShellIntegrations/Commandlets/Serialization/GetPresetCommand.cs
@@ -5,6 +5,7 @@ using System.Xml;
 using Sitecore.Configuration;
 using Sitecore.Data.Items;
 using Sitecore.Data.Serialization.Presets;
+using Sitecore.Shell.Applications.Layouts.PageDesigner.Commands;
 
 namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Serialization
 {
@@ -13,22 +14,50 @@ namespace Cognifide.PowerShell.PowerShellIntegrations.Commandlets.Serialization
     public class GetPresetCommand : BaseCommand
     {
         [Parameter(Position = 0, Mandatory = false)]
-        public string Name { get; set; }
+        public string[] Name { get; set; }
 
         protected override void ProcessRecord()
         {
-            WriteObject(GetPresetItems(Name), true);
-        }
+            //WriteObject(GetPresetItems(Name), true);
 
-        private static IEnumerable<IncludeEntry> GetPresetItems(string name)
-        {
-            IEnumerable<XmlNode> presets = Factory.GetConfigNode("serialization").Cast<XmlNode>();
-            if (name != null)
+            var presetsNode = Factory.GetConfigNode("serialization");
+            if (presetsNode == null)
             {
-                presets = presets.Where(p => p.Name == name);
+                return;
             }
 
-            return presets.SelectMany(PresetFactory.Create);
+            var allPresets = presetsNode.Cast<XmlNode>().ToList();
+
+            Dictionary<string, XmlNode> presets = new Dictionary<string, XmlNode>();
+            if (Name != null && Name.Length > 0)
+            {
+                List<XmlNode> matchingPresets = new List<XmlNode>();
+                foreach (var name in Name)
+                {
+                    matchingPresets.AddRange(WildcardFilter(name, allPresets, p => p.Name));
+                }
+                foreach (var preset in matchingPresets.Where(preset => !presets.ContainsKey(preset.Name)))
+                {
+                    presets.Add(preset.Name, preset);
+                }
+                WriteObject(presets.Values.SelectMany(CreatePreset), true);
+            }
+            else
+            {
+                WriteObject(allPresets.SelectMany(CreatePreset), true);
+            }
+
+        }
+
+        private IEnumerable<PSObject> CreatePreset(XmlNode presetNode)
+        {
+            IList<IncludeEntry> presetEntries = PresetFactory.Create(presetNode);
+            return presetEntries.Select(preset =>
+            {
+                PSObject psPreset = PSObject.AsPSObject(preset);
+                psPreset.Properties.Add(new PSNoteProperty("PresetName", presetNode.Name));
+                return psPreset;
+            });
         }
     }
 }
