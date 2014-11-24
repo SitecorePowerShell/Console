dbedb50224cdbd48bff011a5ccbd3581fb69db4a
Adam Najmanowicz <adam@najmanowicz.com>
2014-11-25 00:19:02 +0100

    Start work on SPE modules

---
+++ PowerShellIntegrations/Modules/ModuleManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cognifide.PowerShell.PowerShellIntegrations.Modules
+{
+    public class ModuleManager
+    {
+    }
+}
