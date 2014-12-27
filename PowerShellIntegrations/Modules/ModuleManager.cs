fc20d2fa155a1d792d313917653d8247c653173a
Adam Najmanowicz <adam@najmanowicz.com>
2014-12-27 18:04:43 +0100

    Issue #264 - Purge old DMS commandlets

--- PowerShellIntegrations/Modules/ModuleManager.cs
+++ PowerShellIntegrations/Modules/ModuleManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Cognifide.PowerShell.PowerShellIntegrations.Settings;
-using Microsoft.SqlServer.Server;
 using Sitecore.Configuration;
 using Sitecore.Data.Items;
 using Sitecore.Install.Items;
