7b70b41df688e887e43d50a350c1646f7fd466f0
Adam Najmanowicz <adam@najmanowicz.com>
2015-02-24 23:46:45 +0100

    Issue #306

--- Commandlets/Session/InvokeScriptCommand.cs
+++ Commandlets/Session/InvokeScriptCommand.cs
@@ -14,16 +14,12 @@ namespace Cognifide.PowerShell.Commandlets.Session
     {
         private const string ParameterSetNameFromItem = "From Item";
         private const string ParameterSetNameFromFullPath = "From Full Path";
-        private const string ParameterSetNameFromCurrentLocation = "From Current Database Library";
-        private const string ParameterSetNameFromContentDatabase = "From Content Database Library";
 
         [Parameter(ParameterSetName = ParameterSetNameFromItem, ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true, Mandatory = true, Position = 0)]
         public Item Item { get; set; }
 
         [Parameter(ParameterSetName = ParameterSetNameFromFullPath, Mandatory = true, Position = 0)]
-        [Parameter(ParameterSetName = ParameterSetNameFromCurrentLocation, Mandatory = true, Position = 0)]
-        [Parameter(ParameterSetName = ParameterSetNameFromContentDatabase, Mandatory = true, Position = 0)]
         [Alias("FullName", "FileName")]
         public string Path { get; set; }
 
@@ -37,20 +33,14 @@ namespace Cognifide.PowerShell.Commandlets.Session
             }
             else if (Path != null)
             {
-                Item curItem = null;
-                if (ParameterSetName == ParameterSetNameFromFullPath ||
-                    ParameterSetName == ParameterSetNameFromContentDatabase)
-                {
-                    curItem = PathUtilities.GetItem(Path, Context.ContentDatabase.Name, ApplicationSettings.ScriptLibraryPath);
-                }
-                if (curItem == null || ParameterSetName == ParameterSetNameFromCurrentLocation)
-                {
-                    curItem = PathUtilities.GetItem(Path, CurrentDatabase.Name, ApplicationSettings.ScriptLibraryPath);
-                }
+                var drive = IsCurrentDriveSitecore ? CurrentDrive : ApplicationSettings.ScriptLibraryDb;
+
+                Item curItem = PathUtilities.GetItem(Path, drive, ApplicationSettings.ScriptLibraryPath);
 
                 if (curItem == null)
                 {
-                    WriteError(new ErrorRecord(new ItemNotFoundException(string.Format("Script '{0}' not found.", Path)),
+                    WriteError(new ErrorRecord(
+                        new ItemNotFoundException(string.Format("Script '{0}' not found.", Path)),
                         "sitecore_script_missing", ErrorCategory.ObjectNotFound, null));
                 }
                 script = curItem["script"];
