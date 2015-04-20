1ef716e8465126a1b64bde6d53c6e79c1f413fcb
Michael West <michaellwest@gmail.com>
2015-04-19 21:19:24 -0500

    Added command to create a new session and return the result.

---
+++ Commandlets/Session/NewScriptSessionCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using Cognifide.PowerShell.Core.Host;
+using Cognifide.PowerShell.Core.Settings;
+
+namespace Cognifide.PowerShell.Commandlets.Session
+{
+    [Cmdlet(VerbsCommon.New, "ScriptSession", DefaultParameterSetName = "All")]
+    public class NewScriptSessionCommand : BaseCommand
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(ScriptSessionManager.NewSession(ApplicationNames.RemoteAutomation, false));
+        }
+    }
+}
