2df61230e597b0f809f2458466878164a77543a2
Michael West <michaellwest@gmail.com>
2015-06-09 07:55:36 -0500

    Fixed issue where getting the version number results in an error for issue #369.

--- Cognifide.PowerShell/Core/VersionDecoupling/VersionResolver.cs
+++ Cognifide.PowerShell/Core/VersionDecoupling/VersionResolver.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Sitecore.Configuration;
 
 namespace Cognifide.PowerShell.Core.VersionDecoupling
 {
     public class VersionResolver
     {
-        public static Version SitecoreVersionCurrent = Version.Parse(About.Version);
+        public static Version SitecoreVersionCurrent = GetVersionNumber(About.Version);
         public static Version SitecoreVersion72 = new Version(7, 2);
         public static Version SitecoreVersion75 = new Version(7, 5);
         public static Version SitecoreVersion80 = new Version(8, 0);
@@ -16,6 +13,17 @@ namespace Cognifide.PowerShell.Core.VersionDecoupling
         public static bool IsVersionHigherOrEqual(Version version)
         {
             return version <= SitecoreVersionCurrent;
+        }
+
+        public static Version GetVersionNumber(string version)
+        {
+            Version result;
+            if (Version.TryParse(version, out result))
+            {
+                return result;
+            }
+
+            return Version.TryParse(About.GetVersionNumber(false), out result) ? result : new Version(0, 0);
         }
     }
 }
