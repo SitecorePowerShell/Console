5eff2dbee8aa678bdbb91f5f5f0140aa3a39fa2c
Adam Najmanowicz <adam@najmanowicz.com>
2015-06-09 20:35:34 +0200

    Issue #369 - started this implementation before I saw your fix Mikey - adding my additional resolution based on assembly version since I already had it in my solution.

--- Cognifide.PowerShell/Core/VersionDecoupling/VersionResolver.cs
+++ Cognifide.PowerShell/Core/VersionDecoupling/VersionResolver.cs
@@ -1,29 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
 using Sitecore.Configuration;
 
 namespace Cognifide.PowerShell.Core.VersionDecoupling
 {
     public class VersionResolver
     {
-        public static Version SitecoreVersionCurrent = GetVersionNumber(About.Version);
+        public static Version SitecoreVersionCurrent = GetVersionNumber();
         public static Version SitecoreVersion72 = new Version(7, 2);
         public static Version SitecoreVersion75 = new Version(7, 5);
         public static Version SitecoreVersion80 = new Version(8, 0);
 
+        public static Version GetVersionNumber()
+        {
+            Version version;
+            if (Version.TryParse(About.Version, out version))
+            {
+                return version;
+            }
+            if (Version.TryParse(About.GetVersionNumber(false), out version))
+            {
+                return version;
+            }
+            var kernel = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName()
+                    .Name.Equals("Sitecore.Kernel", StringComparison.OrdinalIgnoreCase));
+
+            if (kernel != null)
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(kernel.Location);
+                if (!Version.TryParse(fvi.FileVersion, out version))
+                {
+                    version = new Version(7, 0);
+                }
+            }
+            else
+            {
+                version = new Version(7, 0);
+            }
+            return version;
+        }
+
         public static bool IsVersionHigherOrEqual(Version version)
         {
             return version <= SitecoreVersionCurrent;
-        }
-
-        public static Version GetVersionNumber(string version)
-        {
-            Version result;
-            if (Version.TryParse(version, out result))
-            {
-                return result;
-            }
-
-            return Version.TryParse(About.GetVersionNumber(false), out result) ? result : new Version(0, 0);
         }
     }
 }
