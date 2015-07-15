3b8d50273f737f875a070389c6f3d1e6a4e5a1be
Adam Najmanowicz <adam@najmanowicz.com>
2015-07-15 09:13:14 +0200

    Issue #411

--- Cognifide.PowerShell/Client/Controls/MruGallery.cs
+++ Cognifide.PowerShell/Client/Controls/MruGallery.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using Cognifide.PowerShell.Core.Settings;
 using Sitecore;
 using Sitecore.Configuration;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
-using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Globalization;
+using Sitecore.Resources;
 using Sitecore.Shell.Applications.ContentManager.Galleries;
-using Sitecore.Shell.Framework.Commands;
 using Sitecore.Web;
+using Sitecore.Web.UI;
 using Sitecore.Web.UI.HtmlControls;
 using Sitecore.Web.UI.Sheer;
 using Sitecore.Web.UI.WebControls;
+using Sitecore.Web.UI.XmlControls;
+using Control = Sitecore.Web.UI.HtmlControls.Control;
+using ListItem = Sitecore.Web.UI.HtmlControls.ListItem;
+using Menu = Sitecore.Web.UI.HtmlControls.Menu;
+using MenuItem = Sitecore.Web.UI.HtmlControls.MenuItem;
 
 namespace Cognifide.PowerShell.Client.Controls
 {
@@ -25,11 +37,14 @@ namespace Cognifide.PowerShell.Client.Controls
         protected GalleryMenu Options;
         protected Menu RecentMenu;
         protected Tab RecentTab;
-
+        protected Scrollbox Scripts;
+        protected Edit SearchPhrase;
+        protected MenuHeader RecentHeader;
+        protected GalleryMenu SearchResults;
         public override void HandleMessage(Message message)
         {
             Assert.ArgumentNotNull(message, "message");
-            if (message.Name != "event:click" && message.Name != "datacontext:changed" && message.Name != "event:change")
+            if (message.Name != "event:click" && message.Name != "datacontext:changed" && message.Name != "event:change" && message.Name != "event:keypress")
             {
                 Invoke(message, true);
             }
@@ -80,8 +95,8 @@ namespace Cognifide.PowerShell.Client.Controls
                 Item item;
                 Item item2;
                 Item[] itemArray;
-                RenderRecent();
-
+                ChangeSearchPhrase();
+                
                 var db = WebUtil.GetQueryString("contextDb");
                 var itemId = WebUtil.GetQueryString("contextItem");
                 InitialDatabase =
@@ -111,47 +126,6 @@ namespace Cognifide.PowerShell.Client.Controls
                 {
                     ContentDataContext.Folder = itemArray[0].ID.ToString();
                 }
-                /*
-                                var menuItem =
-                                    Sitecore.Client.CoreDatabase.GetItem(
-                                        "/sitecore/content/Applications/PowerShell/PowerShellIse/Menus/Context");
-                                if ((menuItem != null) && item.HasChildren)
-                                {
-                                    var queryString = WebUtil.GetQueryString("id");
-                                    Options.AddFromDataSource(menuItem, queryString, new CommandContext(menuItem));
-                                }
-                */
-            }
-        }
-
-        private void RenderRecent()
-        {
-            SheerResponse.DisableOutput();
-            try
-            {
-                foreach (Item item in ApplicationSettings.GetIseMruContainerItem().Children)
-                {
-                    var messageString = item["Message"];
-                    var message = Message.Parse(null, messageString);                    
-                    var db = message.Arguments["db"];
-
-                    if (item != null)
-                    {
-                        var child = new MenuItem();
-                        RecentMenu.Controls.Add(child);
-                        child.Header = item.DisplayName + (db == Context.ContentDatabase.Name ? "" : " [" + db + "]");
-                        child.Icon = item.Appearance.Icon;
-                        child.Click = "ExecuteMruItem(\"" + item["Message"] + "\")";
-                    }
-                }
-                if (RecentMenu.Controls.Count == 0)
-                {
-                    RecentMenu.Add("(empty)", string.Empty, string.Empty);
-                }
-            }
-            finally
-            {
-                SheerResponse.EnableOutput();
             }
         }
 
@@ -179,6 +153,127 @@ namespace Cognifide.PowerShell.Client.Controls
             ContentDataContext.Parameters = "databasename=" + name;
             ContentDataContext.EndUpdate();
             ContentTreeview.RefreshRoot();
+        }
+
+        protected void ChangeSearchPhrase()
+        {
+            var parts = (SearchPhrase.Value ?? string.Empty).Split(':');
+            var database = parts.Length > 1 ? parts[0] : string.Empty;
+            var phrase = parts.Length > 1 ? parts[1] : parts[0];
+            RecentHeader = new MenuHeader();
+            SearchResults.Controls.AddAt(0,RecentHeader);
+            Scripts.Controls.Clear();
+            Scripts.Class = Scripts.Class + " scDontStretch";
+            SearchResults.Class = Scripts.Class + " scDontStretch";
+            if (string.IsNullOrEmpty(phrase))
+            {
+                RecentHeader.Header = "Most Recently opened scripts:";
+                foreach (Item item in ApplicationSettings.GetIseMruContainerItem().Children)
+                {
+                    var messageString = item["Message"];
+                    var message = Message.Parse(null, messageString);
+                    var db = message.Arguments["db"];
+                    var id = message.Arguments["id"];
+                    var scriptItem = Factory.GetDatabase(db).GetItem(id);
+                    if (scriptItem != null)
+                    {
+                        RenderRecent(scriptItem);
+                    }
+                }
+                
+            }
+            else if (database.Length > 0)
+            {
+                RecentHeader.Header = "Scripts matching: '" + phrase + "' in '" + database + "*' database";
+                foreach (var index in ContentSearchManager.Indexes)
+                {
+                    if (index.Name.StartsWith("sitecore_"+database) &&
+                        index.Name.EndsWith("_index"))
+                    {
+                        SearchDatabase(index.Name, phrase);
+                    }
+                }
+            }
+            else
+            {
+                RecentHeader.Header = "Scripts matching: '" + phrase + "' in all databases";
+                var masterIndex = "sitecore_" + ApplicationSettings.ScriptLibraryDb + "_index";
+                SearchDatabase(masterIndex, phrase);
+                foreach (var index in ContentSearchManager.Indexes)
+                {
+                    if (!string.Equals(masterIndex, index.Name, StringComparison.OrdinalIgnoreCase) && index.Name.StartsWith("sitecore_") &&
+                        index.Name.EndsWith("_index"))
+                    {
+                        SearchDatabase(index.Name, phrase);
+                    }
+                }
+            }
+            HtmlTextWriter writer = new HtmlTextWriter(new StringWriter());
+            SearchResults.RenderControl(writer);
+            SheerResponse.SetOuterHtml(SearchResults.ID, writer.InnerWriter.ToString());
+        }
+
+        private void SearchDatabase(string indexName, string phrase)
+        {
+            using (
+                var context =
+                    ContentSearchManager.GetIndex(indexName)
+                        .CreateSearchContext())
+            {
+                // get all items in medialibrary
+                var rootID = ApplicationSettings.ScriptLibraryRoot().ID.ToShortID().ToString();
+                var query =
+                    context.GetQueryable<SearchResultItem>()
+                        .Where(
+                            i =>
+                                i["_path"].Contains(rootID) &&
+                                i["_templatename"] == "PowerShell Script").Take(10);
+                if (!string.IsNullOrWhiteSpace(phrase))
+                {
+                    query = query.Where(i => i["_name"].Contains(phrase));
+                }
+                foreach (var result in query)
+                {
+                    var scriptItem = result.GetItem();
+                    RenderRecent(scriptItem);
+                }
+            }
+        }
+
+        private void RenderRecent(Item scriptItem)
+        {
+            var control = ControlFactory.GetControl("MruGallery.SearchItem") as XmlControl;
+            Assert.IsNotNull(control, typeof (XmlControl), "Xml Control \"{0}\" not found",
+                "MruGallery.SearchItem");
+            Context.ClientPage.AddControl(Scripts, control);
+            var icon = scriptItem.Appearance.Icon;
+            var iconUrl = string.Empty;
+            if (!string.IsNullOrWhiteSpace(icon))
+            {
+                var builder = new ImageBuilder
+                {
+                    Src = Images.GetThemedImageSource(icon, ImageDimension.id16x16),
+                    Class = "scRibbonToolbarSmallGalleryButtonIcon",
+                    Alt = scriptItem.DisplayName
+                };
+                iconUrl = builder.ToString();
+            }
+            if (false) //session.ID == CurrenSession)
+            {
+                iconUrl = "<div class=\"versionNumSelected\">" + iconUrl + "</div>";
+            }
+            else
+            {
+                iconUrl = "<div class=\"versionNum\">" + iconUrl + "</div>";
+            }
+
+            control["ScriptIcon"] = iconUrl;
+            control["Location"] = Translate.Text("{0}",
+                scriptItem.Paths.ParentPath.Substring(ApplicationSettings.ScriptLibraryPath.Length));
+            control["Database"] = Translate.Text("{0}", scriptItem.Database.Name);
+            control["Name"] = Translate.Text("{0}", scriptItem.DisplayName);
+            control["Click"] = string.Format("ExecuteMruItem(\"ise:mruopen(id={0},db={1})\")", scriptItem.ID,
+                scriptItem.Database.Name);
         }
     }
 }
