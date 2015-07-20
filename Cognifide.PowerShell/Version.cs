387b2ce890f0ca866710c0e2e3507902968c9110
Michael West <michaellwest@gmail.com>
2015-07-19 22:39:54 -0500

    Updated project to version 3.2 and consolidated the assembly details.

---
+++ Cognifide.PowerShell/Version.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyInformationalVersion("3.2.0.0")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cognifide Limited")]
+[assembly: AssemblyProduct("Sitecore PowerShell Extensions")]
+[assembly: AssemblyCopyright("© 2010-2015 Adam Najmanowicz - Cognifide Limited, Michael West. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+
+[assembly: ComVisible(false)]
