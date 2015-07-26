69cee7d6c2215e59496048ca7548670537c74dbc
adamnaj <adam@najmanowicz.com>
2015-07-26 05:53:11 +0200

    Fix for MultilistEx editor not working for Read-Variable on Sitecore 7.0 - due to API Changes.

---
+++ Cognifide.PowerShell/Client/Controls/MultilistExtended.cs
@@ -0,0 +1,22 @@
+﻿using Cognifide.PowerShell.Core.VersionDecoupling;
+using Sitecore.Shell.Applications.ContentEditor;
+
+namespace Cognifide.PowerShell.Client.Controls
+{
+    internal class MultilistExtended : MultilistEx
+    {
+        public virtual void SetLanguage(string language)
+        {
+            if (VersionResolver.IsVersionHigherOrEqual(VersionResolver.SitecoreVersion75))
+            {
+                SetLanguageInternal(language);
+            }
+        }
+
+        private void SetLanguageInternal(string language)
+        {
+            ItemLanguage = language;
+        }
+
+    }
+}
