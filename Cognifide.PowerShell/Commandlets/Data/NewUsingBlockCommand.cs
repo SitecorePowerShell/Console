bb9b66be50502826d833b7a13c706b8b8afab223
Michael West <michaellwest@gmail.com>
2015-09-19 17:44:14 -0500

    #502 : Added cmdlet New-UsingBlock and removed the function.

---
+++ Cognifide.PowerShell/Commandlets/Data/NewUsingBlockCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Cognifide.PowerShell.Commandlets.Data
+{
+    [Cmdlet(VerbsCommon.New, "UsingBlock")]
+    public class NewUsingBlockCommand : BaseCommand
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public IDisposable InputObject { get; set; }
+
+        [Parameter(Mandatory = true, Position = 1)]
+        public ScriptBlock ScriptBlock { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                using (InputObject)
+                {
+                    WriteObject(ScriptBlock.Invoke());
+                }
+            }
+            catch
+            {
+                InputObject?.Dispose();
+            }
+        }
+    }
+}
