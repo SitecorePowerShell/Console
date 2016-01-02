db028b46f74f4d3f2c9e512299d80f57323fdfd7
adamnaj <adam@najmanowicz.com>
2016-01-02 08:15:46 +0100

    Issue #546 WIP

--- Cognifide.PowerShell/Integrations/Pipelines/PageEditorExperienceButtonScript.cs
+++ Cognifide.PowerShell/Integrations/Pipelines/PageEditorExperienceButtonScript.cs
@@ -3,18 +3,30 @@ using System.Linq;
 using Cognifide.PowerShell.Core.Host;
 using Cognifide.PowerShell.Core.Modules;
 using Cognifide.PowerShell.Core.Settings;
+using Cognifide.PowerShell.Core.Utility;
+using Sitecore.Data.Fields;
 using Sitecore.Diagnostics;
 using Sitecore.Pipelines.GetChromeData;
+using Sitecore.Rules;
 
 namespace Cognifide.PowerShell.Integrations.Pipelines
 {
-    public class PageEditorExperienceButtonScript
+    public class PageEditorExperienceButtonScript : GetChromeDataProcessor
     {
         public string IntegrationPoint => IntegrationPoints.PageEditorExperienceButtonFeature;
 
-        public void Process(GetChromeDataArgs args)
+        public override void Process(GetChromeDataArgs args)
         {
             Assert.ArgumentNotNull(args, "args");
+
+            var ruleContext = new RuleContext
+            {
+                Item = args.Item
+            };
+            foreach (var parameter in args.CommandContext.Parameters.AllKeys)
+            {
+                ruleContext.Parameters[parameter] = args.CommandContext.Parameters[parameter];
+            }
 
             foreach (var libraryItem in ModuleManager.GetFeatureRoots(IntegrationPoint))
             {
@@ -22,20 +34,38 @@ namespace Cognifide.PowerShell.Integrations.Pipelines
 
                 foreach (var scriptItem in libraryItem.Children.ToList())
                 {
-                    using (var session = ScriptSessionManager.NewSession(ApplicationNames.Default, true))
+                    if (!RulesUtils.EvaluateRules(scriptItem["ShowRule"], ruleContext))
                     {
-                        session.SetVariable("pipelineArgs", args);
-
-                        try
-                        {
-                            session.SetItemLocationContext(args.Item);
-                            session.ExecuteScriptPart(scriptItem, false);
-                        }
-                        catch (Exception ex)
-                        {
-                            Log.Error(ex.Message, this);
-                        }
+                        continue;
                     }
+
+                    AddButtonsToChromeData(new[]
+                    {
+                        new WebEditButton
+                        {
+                            Click = "webedit:fieldeditor(fields={0}, command={{007A0E9E-59AA-48BB-84F2-6D25A8D2EF80}})",
+                            Icon = scriptItem.Appearance.Icon,
+                            Tooltip = scriptItem.Name,
+                            Header = scriptItem.Name,
+                            Type = "sticky" // sticky keeps it from being hidden in the 'more' dropdown
+                        }
+                    }, args);
+                    /*
+                                        using (var session = ScriptSessionManager.NewSession(ApplicationNames.Default, true))
+                                        {
+                                            session.SetVariable("pipelineArgs", args);
+
+                                            try
+                                            {
+                                                session.SetItemLocationContext(args.Item);
+                                                session.ExecuteScriptPart(scriptItem, false);
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                Log.Error(ex.Message, this);
+                                            }
+                                        }
+                    */
                 }
             }
         }
