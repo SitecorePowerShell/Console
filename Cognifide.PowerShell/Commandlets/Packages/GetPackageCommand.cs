10191ecbc7441629297e80b4e8abe55e0d57cae9
Michael West <michaellwest@gmail.com>
2016-05-22 21:20:54 -0500

    #340 : Updated Get-Package to now support reading the project file from a .zip.
    Replaced code in anti-package script to take advantage of the enhanced Get-Package.

--- Cognifide.PowerShell/Commandlets/Packages/GetPackageCommand.cs
+++ Cognifide.PowerShell/Commandlets/Packages/GetPackageCommand.cs
@@ -1,8 +1,12 @@
 ﻿using System.IO;
 using System.Management.Automation;
+using Cognifide.PowerShell.Core.Extensions;
+using Cognifide.PowerShell.Core.Utility;
 using Sitecore.Install;
 using Sitecore.Install.Serialization;
 using Sitecore.IO;
+using Sitecore.Zip;
+using Sitecore.Zip.Utils;
 
 namespace Cognifide.PowerShell.Commandlets.Packages
 {
@@ -30,14 +34,48 @@ namespace Cognifide.PowerShell.Commandlets.Packages
 
                     if (File.Exists(fileName))
                     {
-                        packageProject =
-                            IOUtils.LoadObject(
-                                FileUtil.ReadFromFile(fileName))
-                                as PackageProject;
+                        var isZip = false;
+                        using (var stream = FileUtil.OpenRead(fileName))
+                        {
+                            if (ZipUtils.IsZipContent(stream))
+                            {
+                                isZip = true;
+                            }
+                        }
+
+                        packageProject = !isZip ? IOUtils.LoadSolution(FileUtil.ReadFromFile(fileName)) : ExtractPackageProject(fileName);
                     }
+
                     WriteObject(packageProject, false);
                 }
             });
+        }
+
+        private static PackageProject ExtractPackageProject(string fileName)
+        {
+            PackageProject packageProject = null;
+
+            using (var packageReader = new ZipReader(fileName))
+            {
+                var packageEntry = packageReader.GetEntry("package.zip");
+                using (var memoryStream = new MemoryStream())
+                {
+                    StreamUtils.CopyStream(packageEntry.GetStream(), memoryStream, 0x4000);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new ZipReader(memoryStream))
+                    {
+                        foreach (var entry in reader.Entries)
+                        {
+                            if (!entry.Name.Is("installer/project")) continue;
+
+                            packageProject = IOUtils.LoadSolution(StreamUtil.LoadString(entry.GetStream()));
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return packageProject;
         }
     }
 }
