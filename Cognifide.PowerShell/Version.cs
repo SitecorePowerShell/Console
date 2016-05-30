5d681b3db7ad329ad940be52c543c7ccde4cb7dc
adamnaj <adam@najmanowicz.com>
2016-05-30 13:23:11 +0200

    Pre-release preparation - version change

--- Cognifide.PowerShell/Version.cs
+++ Cognifide.PowerShell/Version.cs
@@ -2,9 +2,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.0.0.*")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.*")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cognifide Limited")]
