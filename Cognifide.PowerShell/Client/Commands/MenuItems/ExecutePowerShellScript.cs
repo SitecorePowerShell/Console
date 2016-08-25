c9bddb5e85af831970ec9eef11620e2144a1c1da
adamnaj <adam@najmanowicz.com>
2016-08-25 18:49:56 +0200

    Issue #686

--- Cognifide.PowerShell/Client/Commands/MenuItems/ExecutePowerShellScript.cs
+++ Cognifide.PowerShell/Client/Commands/MenuItems/ExecutePowerShellScript.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
 using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Text;
 using Sitecore.Web.UI.Sheer;
@@ -10,6 +12,13 @@ namespace Cognifide.PowerShell.Client.Commands.MenuItems
     [Serializable]
     public class ExecutePowerShellScript : Command
     {
+        private string scriptId;
+        private string scriptDb;
+        private string itemId;
+        private string itemDb;
+        private string itemLang;
+        private string itemVer;
+
         public override CommandState QueryState(CommandContext context)
         {
             return context.Parameters["ScriptRunning"] != "1" ? CommandState.Enabled : CommandState.Disabled;
@@ -17,32 +26,45 @@ namespace Cognifide.PowerShell.Client.Commands.MenuItems
 
         public override void Execute(CommandContext context)
         {
-            var scriptId = context.Parameters["script"];
-            var scriptDb = context.Parameters["scriptDb"];
-
-            var itemId = string.Empty;
-            var itemDb = string.Empty;
-            var itemLang = string.Empty;
-            var itemVer = string.Empty;
-
+            Assert.ArgumentNotNull(context, "context");
+            scriptId = context.Parameters["script"];
+            scriptDb = context.Parameters["scriptDb"];
             if (context.Items.Length > 0)
             {
-                itemId = context.Items[0].ID.ToString();
-                itemDb = context.Items[0].Database.Name;
-                itemLang = context.Items[0].Language.Name;
-                itemVer = context.Items[0].Version.Number.ToString(CultureInfo.InvariantCulture);
-            }
+                var item = context.Items[0];
+                itemId = item.ID.ToString();
+                itemDb = item.Database.Name;
+                itemLang = item.Language.Name;
+                itemVer = item.Version.Number.ToString(CultureInfo.InvariantCulture);
 
-            var str = new UrlString(UIUtil.GetUri("control:PowerShellRunner"));
-            str.Append("id", itemId);
-            str.Append("db", itemDb);
-            str.Append("lang", itemLang);
-            str.Append("ver", itemVer);
-            str.Append("scriptId", scriptId);
-            str.Append("scriptDb", scriptDb);
-            Context.ClientPage.ClientResponse.Broadcast(
-                SheerResponse.ShowModalDialog(str.ToString(), "400", "260", "PowerShell Script Results", false),
-                "Shell");
+            }
+            SheerResponse.CheckModified(false);
+            Context.ClientPage.Start(this, "Process");
+        }
+
+        protected void Process(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (args.IsPostBack)
+            {
+                Context.ClientPage.SendMessage(this, $"item:refresh(id={itemId})");
+                Context.ClientPage.SendMessage(this, $"item:refreshchildren(id={itemId})");
+            }
+            else
+            {
+                var str = new UrlString(UIUtil.GetUri("control:PowerShellRunner"));
+                str.Append("id", itemId);
+                str.Append("db", itemDb);
+                str.Append("lang", itemLang);
+                str.Append("ver", itemVer);
+                str.Append("scriptId", scriptId);
+                str.Append("scriptDb", scriptDb);
+                //Context.ClientPage.ClientResponse.Broadcast(
+                    SheerResponse.ShowModalDialog(str.ToString(), "400", "260", "PowerShell Script Results", true)
+                //    ,"Shell")
+                ;
+                args.WaitForPostBack();
+            }
         }
     }
 }
