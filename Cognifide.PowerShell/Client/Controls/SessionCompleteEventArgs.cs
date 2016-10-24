3aa05206c030ccb827de8200eab0ca85d3d4f4ca
adamnaj <adam@najmanowicz.com>
2016-10-25 00:37:24 +0200

    Missing from previous commit

---
+++ Cognifide.PowerShell/Client/Controls/SessionCompleteEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Cognifide.PowerShell.Client.Applications;
+
+namespace Cognifide.PowerShell.Client.Controls
+{
+    public class SessionCompleteEventArgs : EventArgs
+    {
+        public RunnerOutput RunnerOutput { get; set; }
+    }
+}
