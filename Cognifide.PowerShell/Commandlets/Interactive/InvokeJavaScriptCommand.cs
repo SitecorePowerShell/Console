c555d893fa3ecb02396bb9395d97761b6dfb45bf
adamnaj <adam@najmanowicz.com>
2017-01-22 18:02:27 +0100

    Issue #803

---
+++ Cognifide.PowerShell/Commandlets/Interactive/InvokeJavaScriptCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Automation;
+using Cognifide.PowerShell.Commandlets.Interactive.Messages;
+using Sitecore.Jobs.AsyncUI;
+
+namespace Cognifide.PowerShell.Commandlets.Interactive
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "JavaScript")]
+    public class InvokeJavaScriptCommand : BaseShellCommand
+    {
+        [Parameter(ValueFromPipeline = true, Position = 0, Mandatory = true)]
+        public string Script { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            if (!CheckSessionCanDoInteractiveAction()) return;
+
+            LogErrors(() => PutMessage(new InvokeJavaScriptMessage(Script)));
+        }
+    }
+}
