6ffdffd2d72466090d25bfbe6e963af7868567eb
Michael West <michaellwest@gmail.com>
2017-02-12 20:53:38 -0600

    #805 : Minor formatting adjustments.

--- Modules/Remoting Tests - Web Api.Tests.ps1
+++ Modules/Remoting Tests - Web Api.Tests.ps1
@@ -1,44 +1,46 @@
 param(
     [Parameter()]
-    [string]$protocolHost = "http://spe.dev.local"
+    [string]$protocolHost = "https://spe.dev.local"
 )
 
 if(!$protocolHost){
-    $protocolHost = "http://spe.dev.local"
+    $protocolHost = "https://spe.dev.local"
 }
 
-Describe "WebAPi Responses" {
+Describe "Web API Responses" {
     BeforeAll {
         $session = New-ScriptSession -Username "admin" -Password "b" -ConnectionUri $protocolHost
         
         Invoke-RemoteScript -Session $session -ScriptBlock {
-	    $module = Get-Item "master:/system/Modules/PowerShell/Script Library/Getting Started"
-	    $module.Enabled = "1"
+            # Getting Started
+            $module = Get-Item "master:" -ID "{ED2CF34E-1A59-444D-806E-51DB1E560093}"
+            $module.Enabled = "1"
         }
-	Stop-ScriptSession -Session $session
+	    Stop-ScriptSession -Session $session
     }
     AfterAll { 
         $session = New-ScriptSession -Username "admin" -Password "b" -ConnectionUri $protocolHost
         
         Invoke-RemoteScript -Session $session -ScriptBlock {
-	    $module = Get-Item "master:/system/Modules/PowerShell/Script Library/Getting Started"
-	    $module.Enabled = ""
+            # Getting Started
+            $module = Get-Item "master:" -ID "{ED2CF34E-1A59-444D-806E-51DB1E560093}"
+            $module.Enabled = ""
         }
-	Stop-ScriptSession -Session $session
+	    Stop-ScriptSession -Session $session
     }
     Context "POST Methods" {
         It "ChildrenAsJson script should return 6 objects as children to root item" {
-	    $postParams = @{user="admin"; password="b"; depth=1}
+            $postParams = @{user="admin"; password="b"; depth=1}
             $items = Invoke-RestMethod -Uri "$protocolHost/-/script/v2/master/ChildrenAsJson" -method Post -Body $postParams
             $items.Count | Should Be 6
         }
         It "ChildrenAsHtml Script should return XML Document Object" {
-	    $postParams = @{user="admin"; password="b"}
+            $postParams = @{user="admin"; password="b"}
             $html = Invoke-RestMethod -Uri "$protocolHost/-/script/v2/master/ChildrenAsHtml" -method Post -Body $postParams
             $html | Should BeOfType System.Xml.XmlDocument
         }
         It "HomeAndDescendants Script should return JSON object with Success" {
-	    $postParams = @{user="admin"; password="b"}
+            $postParams = @{user="admin"; password="b"}
             $result = Invoke-RestMethod -Uri "$protocolHost/-/script/v2/master/HomeAndDescendants?offset=0&limit=2&fields=(Name,ItemPath,Id)" -method Post -Body $postParams
             $result | Should BeOfType System.Management.Automation.PSCustomObject
             $result.Status | Should Be Success
@@ -56,7 +58,7 @@ Describe "WebAPi Responses" {
             $html | Should BeOfType System.Xml.XmlDocument
         }
     }
-    Context "WebAPi invalid calls" {
+    Context "Web API invalid calls" {
         It "Non existing script should throw exception" {
             $execution = { Invoke-RestMethod -Uri "$protocolHost/-/script/v2/master/NonExistingScript?user=admin&password=b" }
             $execution | Should Throw "(404) Not Found."
