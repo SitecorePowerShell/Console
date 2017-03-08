4af7dc40e0310a18a72948fcc0fd02ea96983c20
adamnaj <adam@najmanowicz.com>
2017-03-08 14:51:45 +0100

    Issue #853

--- Cognifide.PowerShell/Commandlets/Data/Clones/ConvertFromItemCloneCommand.cs
+++ Cognifide.PowerShell/Commandlets/Data/Clones/ConvertFromItemCloneCommand.cs
@@ -2,6 +2,7 @@
 using System.Management.Automation;
 using Cognifide.PowerShell.Core.Utility;
 using Sitecore.Data.Items;
+using Sitecore.Exceptions;
 
 namespace Cognifide.PowerShell.Commandlets.Data.Clones
 {
@@ -9,17 +10,46 @@ namespace Cognifide.PowerShell.Commandlets.Data.Clones
     [OutputType(typeof (Item), ParameterSetName = new[] {"Item from Pipeline", "Item from Path", "Item from ID"})]
     public class ConvertFromItemCloneCommand : BaseLanguageAgnosticItemCommand
     {
+
+        [Parameter]
+        public SwitchParameter Recurse { get; set; }
+
+        [Parameter]
+        public SwitchParameter PassThru { get; set; }
+
         protected override void ProcessItem(Item item)
         {
-            if (!item.IsItemClone)
+            if (ShouldProcess(item.GetProviderPath(),
+                $"Convert item clone {(Recurse ? "and its children " : string.Empty)}to full item"))
             {
-                WriteError(typeof(ArgumentException), "The specified item is not a clone.", ErrorIds.InvalidItemType, ErrorCategory.InvalidArgument, null);
-                return;
+                Unclone(item);
             }
-            if (ShouldProcess(item.GetProviderPath(), "Convert item clone to full item"))
+        }
+
+        private void Unclone(Item item)
+        {
+            if (item.IsClone)
             {
                 var clone = new CloneItem(item);
-                WriteItem(clone.Unclone());
+                var fullItem = clone.Unclone();
+                if (PassThru)
+                {
+                    WriteItem(fullItem);
+                }
+            }
+            else
+            {
+                if (!item.IsItemClone)
+                {
+                    WriteError(typeof(InvalidTypeException), "The specified item is not a clone.", ErrorIds.InvalidItemType, ErrorCategory.InvalidType, null);
+                }
+            }
+            if (Recurse)
+            {
+                foreach (Item child in item.Children)
+                {
+                    Unclone(child);
+                }
             }
         }
     }
