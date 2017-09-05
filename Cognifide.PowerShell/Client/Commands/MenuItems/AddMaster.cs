734aa26ee7c37839f9d713ece2b023d632d618ff
adamnaj <adam@najmanowicz.com>
2017-09-05 03:57:55 +0200

    Issue #860

---
+++ Cognifide.PowerShell/Client/Commands/MenuItems/AddMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using Cognifide.PowerShell.Core.Extensions;
+using Cognifide.PowerShell.Core.Modules;
+using Cognifide.PowerShell.Core.Settings;
+using Cognifide.PowerShell.Core.Settings.Authorization;
+using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Eventing;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.HtmlControls;
+using Sitecore.Web.UI.Sheer;
+
+namespace Cognifide.PowerShell.Client.Commands.MenuItems
+{
+    public class AddMaster : Sitecore.Shell.Framework.Commands.AddMaster
+    {
+        public override void Execute(CommandContext context)
+        {
+            if (context.Items.Length != 1 || !context.Items[0].Access.CanCreate())
+                return;
+            Item item = context.Items[0];
+            var scriptLibDatabase = Factory.GetDatabase(ApplicationSettings.ScriptLibraryDb);
+            var master = context.Parameters["master"];
+            var scriptItem = scriptLibDatabase.GetItem(master);
+            SheerResponse.Timer(
+                scriptItem.IsPowerShellScript()
+                    ? $"item:executescript(id={item.ID},db={item.Database.Name},script={scriptItem.ID},scriptDb={scriptItem.Database.Name})"
+                    : $"item:addmaster:spefallback(master={master},id={item.ID})",
+                10);
+        }
+    }
+}
---
+++ Cognifide.PowerShell/Integrations/Pipelines/GetItemMasters.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cognifide.PowerShell.Client.Commands.MenuItems;
+using Cognifide.PowerShell.Core.Diagnostics;
+using Cognifide.PowerShell.Core.Extensions;
+using Cognifide.PowerShell.Core.Modules;
+using Cognifide.PowerShell.Core.Settings;
+using Cognifide.PowerShell.Core.Settings.Authorization;
+using Cognifide.PowerShell.Core.Utility;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.GetMasters;
+using Sitecore.Web.UI.HtmlControls;
+
+namespace Cognifide.PowerShell.Integrations.Pipelines
+{
+    public class GetItemMasters
+    {
+        public void Process(GetMastersArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            try
+            {
+                if (!ServiceAuthorizationManager.IsUserAuthorized(WebServiceSettings.ServiceExecution, Context.User.Name))
+                {
+                    return;
+                }
+
+
+                var roots = ModuleManager.GetFeatureRoots(IntegrationPoints.ContentEditorInsertItemFeature);
+
+                foreach (var root in ModuleManager.GetFeatureRoots(IntegrationPoints.ContentEditorInsertItemFeature))
+                {
+                    GetMasters(root, args);
+                }
+
+            }
+            catch (Exception e)
+            {
+                PowerShellLog.Error("Exception while getting item Masters", e);
+            }
+        }
+
+        private void GetMasters(Item parent, GetMastersArgs args)
+        {
+            foreach (var scriptItem in parent.Children.Where(p => p.IsPowerShellScript() || p.IsPowerShellLibrary()))
+            {
+                if (!RulesUtils.EvaluateRules(scriptItem[FieldNames.ShowRule], args.Item))
+                {
+                    continue;
+                }
+
+                if (scriptItem.IsPowerShellScript())
+                {
+                    args.Masters.Add(scriptItem);
+                }
+                else
+                {
+                    GetMasters(scriptItem,args);
+                }
+            }
+        }
+    }
+}
