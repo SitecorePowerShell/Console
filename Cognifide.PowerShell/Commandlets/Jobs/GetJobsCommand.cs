6c1582648c5b30ecf83a727996069f95dc5ce51b
Benjamin Vangansewinkel <VGBenjamin@gmail.com>
2017-11-13 14:49:24 +0100

    Add the Get-Jobs command

---
+++ Cognifide.PowerShell/Commandlets/Jobs/GetJobsCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Web;
+
+namespace Cognifide.PowerShell.Commandlets.Jobs
+{
+    [Cmdlet(VerbsCommon.Get, "Jobs")]
+    [OutputType(typeof (Sitecore.Jobs.Job))]
+    public class GetJobsCommand : BaseCommand
+    {
+        protected override void ProcessRecord()
+        {
+            var jobs = Sitecore.Jobs.JobManager.GetJobs();
+            foreach (var job in jobs)
+            {
+                WriteObject(job);
+            }
+        }
+    }
+}
