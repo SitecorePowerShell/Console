7b5a16efd89e5c69b527e54b90782cf06333325e
Unknown <michaellwest@gmail.com>
2018-03-26 14:39:11 -0500

    #962 : Renamed to fit standard.

---
+++ Cognifide.PowerShell/Commandlets/Jobs/GetSitecoreJobCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Management.Automation;
+using Sitecore.StringExtensions;
+
+namespace Cognifide.PowerShell.Commandlets.Jobs
+{
+    [Cmdlet(VerbsCommon.Get, "SitecoreJob")]
+    [OutputType(typeof (Sitecore.Jobs.Job))]
+    public class GetSitecoreJobCommand : BaseCommand
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            if (Name.IsNullOrEmpty())
+            {
+                var jobs = Sitecore.Jobs.JobManager.GetJobs();
+                WriteObject(jobs, true);
+            }
+            else
+            {
+                var job = Sitecore.Jobs.JobManager.GetJob(Name);
+                WriteObject(job);
+            }
+        }
+    }
+}
