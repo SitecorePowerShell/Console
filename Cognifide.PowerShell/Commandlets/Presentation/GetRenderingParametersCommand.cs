eb524f6cea41e5d56cefd52fd1e2ecffd7cfd09b
tomasz.wiernik <tomasz.wiernik@cognifide.com>
2018-11-20 22:30:02 +0100

    Adding Cmdlets for editing RenderingDefinition Parameters

---
+++ Cognifide.PowerShell/Commandlets/Presentation/GetRenderingParametersCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Management.Automation;
+using Sitecore.Layouts;
+
+namespace Cognifide.PowerShell.Commandlets.Presentation
+{
+    [Cmdlet(VerbsCommon.Get, "RenderingParameters")]
+    [OutputType(typeof(Hashtable))]
+    public class GetRenderingParametersCommand : BaseRenderingParametersCommand
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public RenderingDefinition Rendering { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var parameters = GetParameters(Rendering);
+            WriteObject(new Hashtable(parameters.AllKeys.ToDictionary(k => k, k => parameters[k])));
+        }
+    }
+}
