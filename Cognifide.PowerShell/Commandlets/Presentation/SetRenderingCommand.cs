39c688fc77b42bc32e37a61aae4b5bd83293b29b
Michael West <michaellwest@gmail.com>
2018-12-06 21:23:38 -0600

    #1075 : Added parameter to specify device to Set-Rendering command.

--- Cognifide.PowerShell/Commandlets/Presentation/SetRenderingCommand.cs
+++ Cognifide.PowerShell/Commandlets/Presentation/SetRenderingCommand.cs
@@ -13,8 +13,6 @@ namespace Cognifide.PowerShell.Commandlets.Presentation
     [Cmdlet(VerbsCommon.Set, "Rendering", SupportsShouldProcess = true)]
     public class SetRenderingCommand : BaseLayoutCommand
     {
-        private int index = -1;
-
         [Parameter(Mandatory = true)]
         [Alias("Rendering")]
         public RenderingDefinition Instance { get; set; }
@@ -29,91 +27,92 @@ namespace Cognifide.PowerShell.Commandlets.Presentation
         public string DataSource { get; set; }
 
         [Parameter]
-        public int Index
-        {
-            get { return index; }
-            set { index = value; }
-        }
+        public int Index { get; set; } = -1;
+
+        [Parameter]
+        public virtual DeviceItem Device { get; set; }
 
         protected override void ProcessItem(Item item)
         {
-            if (ShouldProcess(item.GetProviderPath(),
-                $"Set '{Instance.UniqueId}' rendering parameters. Rendering is of type: {Instance.ItemID}"))
+            if (!ShouldProcess(item.GetProviderPath(),
+                $"Set '{Instance.UniqueId}' rendering parameters. Rendering is of type: {Instance.ItemID}")) return;
+
+            LayoutField layoutField = item.Fields[LayoutFieldId];
+            if (layoutField == null || string.IsNullOrEmpty(layoutField.Value))
             {
-                LayoutField layoutField = item.Fields[LayoutFieldId];
-                if (layoutField == null || string.IsNullOrEmpty(layoutField.Value))
+                return;
+            }
+
+            var layout = LayoutDefinition.Parse(layoutField.Value);
+
+            DeviceDefinition selectedDevice;
+            RenderingDefinition rendering;
+
+            var availableDevices = Device != null ? new [] { Device } : layout.Devices.ToArray();
+
+            foreach (DeviceDefinition availableDevice in availableDevices)
+            {
+                foreach (var aRendering in availableDevice.Renderings.Cast<RenderingDefinition>()
+                        .Where(aRendering => aRendering.UniqueId == Instance.UniqueId))
                 {
-                    return;
+                    selectedDevice = availableDevice;
+                    rendering = aRendering;
+                    goto RenderingFound;
                 }
+            }
 
-                var layout = LayoutDefinition.Parse(layoutField.Value);
+            return;
 
-                DeviceDefinition device;
-                RenderingDefinition rendering;
+            RenderingFound: //goto label
+            rendering.ItemID = Instance.ItemID;
+            rendering.Placeholder = MyInvocation.BoundParameters.ContainsKey("PlaceHolder")
+                ? PlaceHolder
+                : Instance.Placeholder ?? rendering.Placeholder;
+            rendering.Datasource =
+                !string.IsNullOrEmpty(DataSource)
+                    ? DataSource
+                    : Instance.Datasource;
+            rendering.Cachable = Instance.Cachable;
+            rendering.VaryByData = Instance.VaryByData;
+            rendering.VaryByDevice = Instance.VaryByDevice;
+            rendering.VaryByLogin = Instance.VaryByLogin;
+            rendering.VaryByParameters = Instance.VaryByParameters;
+            rendering.VaryByQueryString = Instance.VaryByQueryString;
+            rendering.VaryByUser = Instance.VaryByUser;
+            rendering.Parameters = Instance.Parameters;
+            rendering.MultiVariateTest = Instance.MultiVariateTest;
+            rendering.Rules = Instance.Rules;
+            rendering.Conditions = Instance.Conditions;
 
-                foreach (DeviceDefinition aDevice in layout.Devices)
+            if (Parameter != null)
+            {
+                var parameters = new UrlString(rendering.Parameters ?? string.Empty);
+                foreach (string name in Parameter.Keys)
                 {
-                    foreach (
-                        RenderingDefinition aRendering in
-                            aDevice.Renderings.Cast<RenderingDefinition>()
-                                .Where(aRendering => aRendering.UniqueId == Instance.UniqueId))
+                    if (parameters.Parameters.AllKeys.Contains(name))
                     {
-                        device = aDevice;
-                        rendering = aRendering;
-                        goto Renderingfound;
+                        parameters.Parameters[name] = Parameter[name].ToString();
+                    }
+                    else
+                    {
+                        parameters.Add(name, Parameter[name].ToString());
                     }
                 }
 
-                return;
-
-                Renderingfound: //goto label
-                rendering.ItemID = Instance.ItemID;
-                rendering.Placeholder = MyInvocation.BoundParameters.ContainsKey("PlaceHolder")
-                    ? PlaceHolder
-                    : Instance.Placeholder ?? rendering.Placeholder;
-                rendering.Datasource =
-                    !string.IsNullOrEmpty(DataSource)
-                        ? DataSource
-                        : Instance.Datasource;
-                rendering.Cachable = Instance.Cachable;
-                rendering.VaryByData = Instance.VaryByData;
-                rendering.VaryByDevice = Instance.VaryByDevice;
-                rendering.VaryByLogin = Instance.VaryByLogin;
-                rendering.VaryByParameters = Instance.VaryByParameters;
-                rendering.VaryByQueryString = Instance.VaryByQueryString;
-                rendering.VaryByUser = Instance.VaryByUser;
-                rendering.Parameters = Instance.Parameters;
-                rendering.MultiVariateTest = Instance.MultiVariateTest;
-                rendering.Rules = Instance.Rules;
-                rendering.Conditions = Instance.Conditions;
-
-                if (Parameter != null)
-                {
-                    var parameters = new UrlString(rendering.Parameters ?? string.Empty);
-                    foreach (string name in Parameter.Keys)
-                        if (parameters.Parameters.AllKeys.Contains(name))
-                        {
-                            parameters.Parameters[name] = Parameter[name].ToString();
-                        }
-                        else
-                        {
-                            parameters.Add(name, Parameter[name].ToString());
-                        }
-                    rendering.Parameters = parameters.ToString();
-                }
-
-                if (Index > -1)
-                {
-                    device.Renderings.Remove(rendering);
-                    device.Insert(index, rendering);
-                }
-
-                item.Edit(p =>
-                {
-                    var outputXml = layout.ToXml();
-                    LayoutField.SetFieldValue(item.Fields[LayoutFieldId], outputXml);
-                });
+                rendering.Parameters = parameters.ToString();
             }
+
+            if (Index > -1)
+            {
+                selectedDevice.Renderings.Remove(rendering);
+                selectedDevice.Insert(Index, rendering);
+            }
+
+            item.Edit(p =>
+            {
+                var outputXml = layout.ToXml();
+                LayoutField.SetFieldValue(item.Fields[LayoutFieldId], outputXml);
+            });
         }
     }
 }
