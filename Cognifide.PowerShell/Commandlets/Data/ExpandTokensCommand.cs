7495d6e3b770d6f53408b20aa5af2beb7dd31596
Michael West <michaellwest@gmail.com>
2018-12-25 23:57:20 -0600

    Cosmetic cleanup.

--- Cognifide.PowerShell/Commandlets/Data/ExpandTokensCommand.cs
+++ Cognifide.PowerShell/Commandlets/Data/ExpandTokensCommand.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Management.Automation;
-using Cognifide.PowerShell.Core.Utility;
+﻿using Cognifide.PowerShell.Core.Utility;
 using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Data.Items;
+using System;
+using System.Management.Automation;
 
 namespace Cognifide.PowerShell.Commandlets.Data
 {
@@ -11,26 +11,25 @@ namespace Cognifide.PowerShell.Commandlets.Data
     [OutputType(typeof (Item))]
     public class ExpandTokenCommand : BaseItemCommand
     {
-        private static readonly MasterVariablesReplacer tokenReplacer = Factory.GetMasterVariablesReplacer();
+        private static readonly MasterVariablesReplacer TokenReplacer = Factory.GetMasterVariablesReplacer();
 
         protected override void ProcessItem(Item item)
         {
-            if (ShouldProcess(item.GetProviderPath(), "Expand tokens"))
-            {
-                Item.Editing.BeginEdit();
-                try
-                {
-                    tokenReplacer.ReplaceItem(Item);
-                    Item.Editing.EndEdit();
-                }
-                catch (Exception ex)
-                {
-                    Item.Editing.CancelEdit();
-                    WriteError(ex.GetType(), "Cannot complete operation.", ErrorIds.InvalidOperation, ErrorCategory.NotSpecified, Item);
-                }
+            if (!ShouldProcess(item.GetProviderPath(), "Expand tokens")) return;
 
-                WriteItem(Item);
+            Item.Editing.BeginEdit();
+            try
+            {
+                TokenReplacer.ReplaceItem(Item);
+                Item.Editing.EndEdit();
             }
+            catch (Exception ex)
+            {
+                Item.Editing.CancelEdit();
+                WriteError(ex.GetType(), "Cannot complete operation.", ErrorIds.InvalidOperation, ErrorCategory.NotSpecified, Item);
+            }
+
+            WriteItem(Item);
         }
     }
 }
