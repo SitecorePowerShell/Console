fed05790a071ae2d9a6ce82f8c28ecb3afac0bfb
Michael West <michaellwest@gmail.com>
2019-01-15 21:37:16 -0600

    #1090 : Added support for group tasks.

--- Cognifide.PowerShell/Integrations/Tasks/ExecuteScriptTask.cs
+++ Cognifide.PowerShell/Integrations/Tasks/ExecuteScriptTask.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Cognifide.PowerShell.Core.Extensions;
 using Cognifide.PowerShell.Core.Host;
 using Cognifide.PowerShell.Core.Settings;
@@ -15,12 +14,38 @@ namespace Cognifide.PowerShell.Integrations.Tasks
         {
             using (var session = ScriptSessionManager.NewSession(ApplicationNames.Default, true))
             {
-                foreach (var scriptItem in items.Where(si => si.IsPowerShellScript() && !string.IsNullOrWhiteSpace(si[Templates.Script.Fields.ScriptBody])))
+                foreach (var item in items)
                 {
-                    if (!RulesUtils.EvaluateRules(scriptItem[Templates.Script.Fields.EnableRule], scriptItem)) continue;
+                    ProcessTaskItem(item, session);
+                }
+            }
+        }
 
-                    session.SetItemLocationContext(scriptItem);
-                    session.ExecuteScriptPart(scriptItem, true);
+        private static void ProcessTaskItem(Item item, ScriptSession session)
+        {
+            var queue = new Queue<Item>();
+            queue.Enqueue(item);
+
+            Item currentItem;
+            while (queue.Count > 0 && (currentItem = queue.Dequeue()) != null)
+            {
+                if (currentItem.IsPowerShellScript())
+                {
+                    if (string.IsNullOrWhiteSpace(currentItem[Templates.Script.Fields.ScriptBody])) continue;
+                    if (!RulesUtils.EvaluateRules(currentItem[Templates.Script.Fields.EnableRule], currentItem)) continue;
+
+                    session.SetItemLocationContext(currentItem);
+                    session.ExecuteScriptPart(currentItem, true);
+                }
+                else if (currentItem.IsPowerShellLibrary() && currentItem.HasChildren)
+                {
+                    if (!RulesUtils.EvaluateRules(currentItem[Templates.Script.Fields.EnableRule], currentItem)) continue;
+
+                    var children = currentItem.Children.ToArray();
+                    foreach (var child in children)
+                    {
+                        queue.Enqueue(child);
+                    }
                 }
             }
         }
