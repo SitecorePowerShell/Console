09a029ca7a7b66b0145d9136559dd681d776e60a
Michael West <michaellwest@gmail.com>
2019-06-18 21:59:35 -0500

    Made changes to deployment to support wdp packages.

---
+++ Modules/Package-WDP.ps1
@@ -0,0 +1,10 @@
+﻿# Make sure to import `Sitecore.Cloud.Cmdlets.dll` and not `Sitecore.Cloud.Cmdlets.psm1`
+Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
+
+$path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0.zip"
+$destination = "C:\Websites\dev.spe\Data\packages"
+ConvertTo-SCModuleWebDeployPackage `
+    -Path $path `
+    -Destination $destination `
+    -Verbose `
+    -Force
