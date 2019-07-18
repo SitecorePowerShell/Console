2befaffe94cb525f7075d6982b66315821fac95c
Michael West <michaellwest@gmail.com>
2019-07-17 20:06:08 -0500

    Updated for 5.1.

--- Modules/Package-WDP.ps1
+++ Modules/Package-WDP.ps1
@@ -1,10 +1,11 @@
 ﻿# Make sure to import `Sitecore.Cloud.Cmdlets.dll` and not `Sitecore.Cloud.Cmdlets.psm1`
 Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 
-$path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0.zip"
-$destination = "C:\Websites\dev.spe\Data\packages"
-ConvertTo-SCModuleWebDeployPackage `
-    -Path $path `
-    -Destination $destination `
-    -Verbose `
-    -Force
+#$path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0.zip"
+#$destination = "C:\Websites\dev.spe\Data\packages"
+
+$destination = "C:\Websites\sc827\Data\packages"
+$path = "C:\Websites\sc827\Data\packages\Sitecore PowerShell Extensions-5.1.zip"
+ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination  -Verbose  -Force
+$path = "C:\Websites\sc827\Data\packages\Sitecore PowerShell Extensions - Authorable Reports-5.1.zip"
+ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination  -Verbose  -Force
