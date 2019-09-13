a2b1b6f9b0960b4e59c5f12fae46db2fd729ca0b
Michael West <michaellwest@gmail.com>
2019-09-12 20:02:51 -0500

    #1136 : Had to remove the processor due to an API conflict.

--- Modules/Package-WDP.ps1
+++ Modules/Package-WDP.ps1
@@ -1,7 +1,7 @@
 ﻿# Make sure to import `Sitecore.Cloud.Cmdlets.dll` and not `Sitecore.Cloud.Cmdlets.psm1`
 Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 
-$path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0-beta5.zip"
+$path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0-beta6.zip"
 $destination = "C:\Websites\dev.spe\Data\packages"
 
 ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination -DisableDacPacOptions '*' -Verbose  -Force
