e7fcef9440ed0b4bab8473673c87a5c23fed8da9
Michael West <michaellwest@gmail.com>
2019-10-04 14:51:06 -0500

    Added commands to generate the SHA256 hash.

--- Modules/SIF/Package-WDP.ps1
+++ Modules/SIF/Package-WDP.ps1
@@ -5,5 +5,8 @@ Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 $path = "C:\Websites\dev.spe\Data\packages\Sitecore.PowerShell.Extensions-6.0.zip"
 $destination = "C:\Websites\dev.spe\Data\packages"
 
-ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination -DisableDacPacOptions '*' -Verbose  -Force
+$wdpPath = ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination -DisableDacPacOptions '*' -Verbose  -Force
 #ConvertTo-SCModuleWebDeployPackage $_.fullname $destinationPath -Exclude $excludePaths -DisableDacPacOptions '*'
+
+Get-FileHash -Path $path -Algorithm SHA256
+Get-FileHash -Path $wdpPath -Algorithm SHA256
