5cbd62e1b115b597b401e607cc47cbc267ed3e4a
Michael West <michaellwest@gmail.com>
2020-01-25 14:53:47 -0600

    Updated environment variables to Windows 1909.

--- startup/createcert.ps1
+++ startup/createcert.ps1
@@ -20,7 +20,8 @@ $certificate = New-SelfSignedCertificate `
     -KeyUsage DigitalSignature, KeyEncipherment, DataEncipherment `
     -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") `
     -KeyExportPolicy Exportable `
-    -KeySpec KeyExchange
+    -KeySpec KeyExchange `
+    -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider"
 $certificatePath = 'Cert:\CurrentUser\My\' + ($certificate.ThumbPrint)
 # create temporary certificate path
 $tmpPath = $PSScriptRoot
--- startup/startup.ps1
+++ startup/startup.ps1
@@ -1,27 +1,39 @@
 Write-Host "Running startup.ps1"
 Import-Module WebAdministration
 
-$siteName = "Default Web Site"
-Write-Host "Checking if $($siteName) has any existing HTTPS bindings"
+$website = "Default Web Site"
+Write-Host "Checking if $($website) has any existing HTTPS bindings"
 
-$hostHeader = ${env:HOST_HEADER}
-if ($null -eq (Get-WebBinding -Name $siteName | Where-Object { $_.BindingInformation -eq "*:80:$($hostHeader)" })) {
-    Write-Host "Adding a new HTTP binding for $($siteName)"
-    $binding = New-WebBinding -Name $siteName -Protocol http -IPAddress * -Port 80 -HostHeader $hostHeader
-} else {
-    Write-Host "HTTP binding for $($siteName) already exists"
+$hostHeaders = "${env:HOST_HEADER}".Split(";", [System.StringSplitOptions]::RemoveEmptyEntries)
+
+function Set-HttpBinding {
+    param(
+        [string]$SiteName,
+        [string]$HostHeader
+    )
+
+    if ($null -eq (Get-WebBinding -Name $siteName | Where-Object { $_.BindingInformation -eq "*:80:$($hostHeader)" })) {
+        Write-Host "Adding a new HTTP binding for $($siteName)"
+        $binding = New-WebBinding -Name $siteName -Protocol http -IPAddress * -Port 80 -HostHeader $hostHeader
+    } else {
+        Write-Host "HTTP binding for $($siteName) already exists"
+    }
+    
+    if ($null -eq (Get-WebBinding -Name $siteName | Where-Object { $_.BindingInformation -eq "*:443:$($hostHeader)" })) {
+        Write-Host "Adding a new HTTPS binding for $($siteName)"
+        $securePassword = (Get-Content -Path C:\startup\cert.password.txt) | ConvertTo-SecureString -AsPlainText -Force
+        $cert = Import-PfxCertificate -Password $securePassword -CertStoreLocation Cert:\LocalMachine\My -FilePath C:\startup\cert.pfx   
+        $thumbprint = $cert.Thumbprint
+        $binding = New-WebBinding -Name $siteName -Protocol https -IPAddress * -Port 443 -HostHeader $hostHeader
+        $binding = Get-WebBinding -Name $siteName -Protocol https
+        $binding.AddSslCertificate($thumbprint, "my");
+    } else {
+        Write-Host "HTTPS binding for $($siteName) already exists"
+    }
 }
 
-if ($null -eq (Get-WebBinding -Name $siteName | Where-Object { $_.BindingInformation -eq "*:443:$($hostHeader)" })) {
-    Write-Host "Adding a new HTTPS binding for $($siteName)"
-    $securePassword = (Get-Content -Path C:\startup\cert.password.txt) | ConvertTo-SecureString -AsPlainText -Force
-    $cert = Import-PfxCertificate -Password $securePassword -CertStoreLocation Cert:\LocalMachine\My -FilePath C:\startup\cert.pfx   
-    $thumbprint = $cert.Thumbprint
-    $binding = New-WebBinding -Name $siteName -Protocol https -IPAddress * -Port 443 -HostHeader $hostHeader
-    $binding = Get-WebBinding -Name $siteName -Protocol https
-    $binding.AddSslCertificate($thumbprint, "my");
-} else {
-    Write-Host "HTTPS binding for $($siteName) already exists"
+foreach($hostheader in $hostHeaders) {
+    Set-HttpBinding -SiteName $website -HostHeader $hostheader
 }
 
-& "C:\tools\entrypoints\iis\Development.ps1" -WatchDirectoryParameters @{ Path = 'C:\\src'; Destination = 'C:\\inetpub\\wwwroot'; ExcludeDirectories = @('App_Data\\Unicorn'); }
+& "C:\tools\entrypoints\iis\Development.ps1"
