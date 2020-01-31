98b9520b7aa56bbf070b0bdd4ffe815934bf1dc9
Michael West <michaellwest@gmail.com>
2020-01-30 19:37:14 -0600

    Increment version number to 6.1.

--- src/Spe/Version.cs
+++ src/Spe/Version.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("6.0.0.*")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.0.0.0")]
+[assembly: AssemblyVersion("6.1.0.*")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyInformationalVersion("6.1.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sitecore PowerShell Extensions Team")]
 [assembly: AssemblyProduct("Sitecore PowerShell Extensions")]
-[assembly: AssemblyCopyright("© 2010-2019 Adam Najmanowicz, Michael West. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2010-2020 Adam Najmanowicz, Michael West. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
