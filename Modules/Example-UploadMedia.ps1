97220e8bc7c799b20122b3e4bfdc3faa77213d97
Michael West <michaellwest@gmail.com>
2020-04-29 07:37:23 -0500

    Added an example for calling the web service manually.

---
+++ Modules/Example-UploadMedia.ps1
@@ -0,0 +1,22 @@
+﻿$serverUrl = "https://spe.dev.local/"
+$file = "C:\Temp\zoom-tiger.jpg"
+
+$filename = [System.IO.Path]::GetFileName($file)
+Add-Type -AssemblyName "System.Web"
+$contentType = [System.Web.MimeMapping]::GetMimeMapping($file)
+
+$url = @(
+    $serverUrl, 
+    "/sitecore modules/PowerShell/Services/RemoteScriptCall.ashx",
+    "?script=/sitecore/media library/Images/$($filename)",
+    "&sc_database=master&apiVersion=media&scriptDb=master"
+) -join ''
+
+$username = "admin"
+$password = "b"
+$authBytes = [System.Text.Encoding]::GetEncoding("iso-8859-1").GetBytes("$($username):$($password)")
+$headers = @{
+    Authorization = "Basic $([System.Convert]::ToBase64String($authBytes))"
+}
+
+Invoke-WebRequest -Uri $url -Method Post -InFile $file -ContentType $contentType -Headers $headers
