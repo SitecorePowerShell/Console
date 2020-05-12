7c759ebfb610445bf863fd11054f322e46007e0e
Michael West <michaellwest@gmail.com>
2020-05-11 21:54:56 -0500

    Cleaned up a commented out line.

--- Modules/SIF/Package-WDP.ps1
+++ Modules/SIF/Package-WDP.ps1
@@ -2,11 +2,10 @@
 # Make sure to import `Sitecore.Cloud.Cmdlets.dll` and not `Sitecore.Cloud.Cmdlets.psm1`
 Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 
-$path = "C:\Projects\Spe\releases\Sitecore.PowerShell.Extensions-6.1.zip"
+$path = "C:\Projects\Spe\releases\Sitecore.PowerShell.Extensions-6.1.1.zip"
 $destination = "C:\Projects\Spe\releases\"
 
 $wdpPath = ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination -DisableDacPacOptions '*' -Verbose  -Force
-#ConvertTo-SCModuleWebDeployPackage $_.fullname $destinationPath -Exclude $excludePaths -DisableDacPacOptions '*'
 
 Get-FileHash -Path $path -Algorithm SHA256
 Get-FileHash -Path $wdpPath -Algorithm SHA256
