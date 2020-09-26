0af30beb036004ef2956ba5715089e14f2c82c5f
Michael West <michaellwest@gmail.com>
2020-09-26 09:31:22 -0500

    Added sample script to validate username and password with Basic token.

---
+++ Modules/Remoting Tests - Basic Token.ps1
@@ -0,0 +1,15 @@
+﻿Clear-Host
+
+$name = 'sitecore\admin'
+$password = 'b'
+$hostname = "https://spe.dev.local"
+
+Import-Module -Name SPE -Force
+$session = New-ScriptSession -Username $name -Password $password -ConnectionUri $hostname
+$watch = [System.Diagnostics.Stopwatch]::StartNew()
+Invoke-RemoteScript -ScriptBlock {
+    $env:COMPUTERNAME
+} -Session $session -Raw
+$watch.Stop()
+$watch.ElapsedMilliseconds / 1000
+Stop-ScriptSession -Session $session
