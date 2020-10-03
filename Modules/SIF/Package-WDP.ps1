175586098ee47a9ce5a91eb4ec01502900f98b25
Michael West <michaellwest@gmail.com>
2020-10-03 01:23:16 -0500

    Output has messages better than before.

--- Modules/SIF/Package-WDP.ps1
+++ Modules/SIF/Package-WDP.ps1
@@ -2,10 +2,15 @@
 # Make sure to import `Sitecore.Cloud.Cmdlets.dll` and not `Sitecore.Cloud.Cmdlets.psm1`
 Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 
-$path = "C:\Projects\Spe\releases\Sitecore.PowerShell.Extensions-6.1.1.zip"
+$path = "C:\Projects\Spe\releases\Sitecore.PowerShell.Extensions-6.2.zip"
 $destination = "C:\Projects\Spe\releases\"
 
 $wdpPath = ConvertTo-SCModuleWebDeployPackage -Path $path  -Destination $destination -DisableDacPacOptions '*' -Verbose  -Force
 
-Get-FileHash -Path $path -Algorithm SHA256
-Get-FileHash -Path $wdpPath -Algorithm SHA256
+$hash = Get-FileHash -Path $path -Algorithm SHA256
+Write-Host "SHA256 for $([System.IO.Path]::GetFileName($path))"
+Write-Host "$($hash.Hash)"
+Write-Host ""
+$hashwdp = Get-FileHash -Path $wdpPath -Algorithm SHA256
+Write-Host "SHA256 for $([System.IO.Path]::GetFileName($wdpPath))"
+Write-Host "$($hashwdp.Hash)"
