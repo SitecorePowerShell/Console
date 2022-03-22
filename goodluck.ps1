acb517d7ce340a2b309ad32d8c007635a1ef2736
Michael West <michaellwest@gmail.com>
2022-03-22 17:58:22 -0500

    Easier to edit the admin account.

--- goodluck.ps1
+++ goodluck.ps1
@@ -16,7 +16,7 @@ Write-Host "Generate packages from running Sitecore instance."
 Import-Module -Name SPE
 
 $sharedSecret = '7AF6F59C14A05786E97012F054D1FB98AC756A2E54E5C9ACBAEE147D9ED0E0DB'
-$name = 'sitecore\PowerShellExtensionsAPI'
+$name = 'sitecore\admin'
 $hostname = "https://spe.dev.local"
 
 # TODO: Generate normal package with dat files instead of items. Maybe use a temporary file name like Sitecore.PowerShell.Extensions-6.3-IAR.temp.zip
@@ -67,16 +67,15 @@ Import-Module -Name "C:\Sitecore\sat\tools\Sitecore.Cloud.Cmdlets.dll"
 $packages = Get-ChildItem -Path $releases -Filter "Sitecore.PowerShell.Extensions-*.*.zip" | Select-Object -ExpandProperty FullName
 $destination = "$($releases)\"
 
+Write-Host "## File Hashes"
 foreach($package in $packages) {
 
-    Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($package))"
-    Write-Host "-----"
+    Write-Host "### $([System.IO.Path]::GetFileNameWithoutExtension($package))"
     $hash = Get-FileHash -Path $package -Algorithm SHA256
     Write-Host "SHA256 for $([System.IO.Path]::GetFileName($package))"
     Write-Host "$($hash.Hash)"
     Write-Host ""
 
-    Write-Host "Converting $($package) into an scwdp file"
     $wdpPath = ConvertTo-SCModuleWebDeployPackage -Path $package  -Destination $destination -DisableDacPacOptions '*' -Force
     
     $hashwdp = Get-FileHash -Path $wdpPath -Algorithm SHA256
