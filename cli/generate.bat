e5b8d48f6d22252ca40599253c48d28b75ae302f
Michael West <michaellwest@gmail.com>
2022-09-20 14:38:33 -0500

    Added the web IAR file to the build process.

--- cli/generate.bat
+++ cli/generate.bat
@@ -2,3 +2,5 @@ cd /D "%~dp0"
 dotnet tool restore
 dotnet sitecore plugin list
 dotnet sitecore itemres create -o _out/spe --overwrite -i Spe.*
+del /f .\_out\items.web.spe.dat 2>nul
+xcopy .\_out\items.master.spe.dat .\_out\items.web.spe.dat* /Y
