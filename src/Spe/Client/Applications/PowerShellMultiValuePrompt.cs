SitecorePowerShell/Console

commit cdc5aa40a2ec9ba3181af7acda0e7f8565fc65b3

Tougher restrictions on input validation.

src/Spe/Client/Applications/PowerShellMultiValuePrompt.cs
@@ -1055,6 +1055,17 @@ namespace Spe.Client.Applications
             var mandatoryVariables = MandatoryVariables.Split(',').ToList();
 
             var scriptVariables = GetVariableValues();
+
+            var xssCleanup = new Regex(@"<script[^>]*>[\s\S]*?</script>|<noscript[^>]*>[\s\S]*?</noscript>|<img.*onerror.*>");
+            foreach (var variable in scriptVariables)
+            {
+                if (xssCleanup.IsMatch(variable["Value"].ToString()))
+                {
+                    SheerResponse.Alert(Texts.PowerShellMultiValuePrompt_InsecureData_error);
+                    return;
+                }
+            }
+
             var varsHashtable = new Hashtable();
             var canClose = true;
             var fieldValidators = FieldValidators;
@@ -1327,6 +1338,7 @@ namespace Spe.Client.Applications
             {
                 result.Add("Value", control.Value.Split('|'));
             }
+
             return result;
         }
 

src/Spe/Client/Controls/PowerShellListView.cs
@@ -240,7 +240,7 @@ namespace Spe.Client.Controls
                             val = $"<div class='checked'><img src='{checkedSource}'/></div>";
                             break;
                         default:
-                            var xssCleanup = new Regex(@"<script[^>]*>[\s\S]*?</script>");
+                            var xssCleanup = new Regex(@"<script[^>]*>[\s\S]*?</script>|<noscript[^>]*>[\s\S]*?</noscript>|<img.*onerror.*>");
                             val = xssCleanup.Replace(val, "<div title='Script tag removed'>&#9888;</div>");
                             break;
                     }

src/Spe/Client/Controls/UserGallery.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Sitecore;
 using Sitecore.Diagnostics;
 using Sitecore.Globalization;
@@ -73,8 +74,11 @@ namespace Spe.Client.Controls
             control["Class"] = (user.Name == CurrentUser) ? "selected" : string.Empty;
             control["UserIcon"] = $"<div class=\"versionNum\">{builder}</div>";
             control["Name"] = Translate.Text("<b>{0}</b>", user.Name);
-            control["FullName"] = Translate.Text("<b>{0}</b>",
-                string.IsNullOrEmpty(user.Profile.FullName) ? user.LocalName : user.Profile.FullName);
+
+            var xssCleanup = new Regex(@"<script[^>]*>[\s\S]*?</script>|<noscript[^>]*>[\s\S]*?</noscript>|<img.*onerror.*>");
+            var fullname = string.IsNullOrEmpty(user.Profile.FullName) ? user.LocalName : user.Profile.FullName;
+            fullname = xssCleanup.Replace(fullname, "<span title='Script tag removed'>&#9888;</span>");
+            control["FullName"] = Translate.Text("<b>{0}</b>", fullname);
             control["Click"] = $"ise:setuser(user={WebUtil.UrlEncode(user.Name).Replace(@"\", @"\\")})";
         }
     }

src/Spe/Texts.cs
@@ -64,6 +64,7 @@ namespace Spe
         public const string PowerShellMultiValuePrompt_GetCheckboxControl_Select_all = "Select all";
         public const string PowerShellMultiValuePrompt_GetCheckboxControl_Unselect_all = "Unselect all";
         public const string PowerShellMultiValuePrompt_GetCheckboxControl_Invert_selection = "Invert selection";
+        public const string PowerShellMultiValuePrompt_InsecureData_error = "One or more field values include insecure data.";
 
         public const string PowerShellSessionElevation_Execution_prevented = "Execution prevented!";
 

commit 221b672bd4a06bfdfb8aef28b074602d31dce672

Fixed issue with data validation.
Caused by cdc5aa40a2ec9ba3181af7acda0e7f8565fc65b3.

src/Spe/Client/Applications/PowerShellMultiValuePrompt.cs
@@ -1060,7 +1060,8 @@ namespace Spe.Client.Applications
             var xssCleanup = new Regex(@"<script[^>]*>[\s\S]*?</script>|<noscript[^>]*>[\s\S]*?</noscript>|<img.*onerror.*>");
             foreach (var variable in scriptVariables)
             {
-                if (xssCleanup.IsMatch(variable["Value"].ToString()))
+                if (variable["Value"] != null &&
+                    xssCleanup.IsMatch(variable["Value"].ToString()))
                 {
                     SheerResponse.Alert(Texts.PowerShellMultiValuePrompt_InsecureData_error);
                     return;