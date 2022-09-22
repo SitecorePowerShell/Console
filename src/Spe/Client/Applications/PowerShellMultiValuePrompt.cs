SitecorePowerShell/Console

commit dd53a2f77c5e06db01abea9aca340f2da58d4c7e

#1278 : I wonder if this setting causes the issue. Appears to work now.

src/Spe/Client/Applications/PowerShellMultiValuePrompt.cs
@@ -575,10 +575,11 @@ namespace Spe.Client.Applications
                 Value = strValue,
                 AllowMultipleSelection = true,
                 DatabaseName = dbName,
-                Database = dbName,
+                //Database = dbName,
                 Source = variable["Source"] as string ?? "/sitecore",
                 DisplayFieldName = variable["DisplayFieldName"] as string ?? "__DisplayName"
             };
+
             treeList.Class += " treePicker";
 
             return treeList;

commit c9bbff7f2bc2ebc5f8980a15c8b724fa27625d58

Fixed issue with null database name.
Probably caused by dd53a2f77c5e06db01abea9aca340f2da58d4c7e.

src/Spe/Client/Applications/PowerShellMultiValuePrompt.cs
@@ -1274,7 +1274,9 @@ namespace Spe.Client.Applications
             {
                 var strIds = treeList.GetValue();
                 var ids = strIds.Split('|');
-                var db = Database.GetDatabase(treeList.DatabaseName);
+                var db = string.IsNullOrEmpty(treeList.DatabaseName)
+                    ? Sitecore.Context.ContentDatabase
+                    : Database.GetDatabase(treeList.DatabaseName);
                 var items = ids.Select(p => db.GetItem(p)).ToList();
                 result.Add("Value", items);
             }