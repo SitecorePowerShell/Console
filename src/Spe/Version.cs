bcb844f88e0c6d42e80c9bc63d6aef191af07c99
Michael West <michaellwest@gmail.com>
2023-01-22 14:58:11 -0600

    Updated to version 7.0

--- src/Spe/Version.cs
+++ src/Spe/Version.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("6.4.0.*")]
-[assembly: AssemblyFileVersion("6.4.0.0")]
-[assembly: AssemblyInformationalVersion("6.4.0.0")]
+[assembly: AssemblyVersion("7.0.0.*")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sitecore PowerShell Extensions Team")]
 [assembly: AssemblyProduct("Sitecore PowerShell Extensions")]
-[assembly: AssemblyCopyright("© 2010-2022 Adam Najmanowicz, Michael West. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2010-2023 Adam Najmanowicz, Michael West. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
