SitecorePowerShell/Console

commit f47fdbce835192ce7c0ced0eef860df5df758099

#1113 - fixed $SitecoreContextItem not being in the language selected in the ISE ribbon upon script execution.

src/Spe/Client/Applications/PowerShellIse.cs
@@ -12,6 +12,7 @@ using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Data.Managers;
 using Sitecore.Diagnostics;
+using Sitecore.Globalization;
 using Sitecore.IO;
 using Sitecore.Mvc.Extensions;
 using Sitecore.Resources;
@@ -158,7 +159,7 @@ namespace Spe.Client.Applications
                 var contextItemId = ContextItemId;
                 return string.IsNullOrEmpty(contextItemId)
                     ? null
-                    : Factory.GetDatabase(ContextItemDb).GetItem(new ID(contextItemId));
+                    : Factory.GetDatabase(ContextItemDb).GetItem(new ID(contextItemId),Language.Parse(CurrentLanguage));
             }
         }
 

commit c7b59247925c1009f4a3dce22797a8ce79a7e9ec

Fix for issue introduced with f47fdbce835192ce7c0ced0eef860df5df758099

src/Spe/Client/Applications/PowerShellIse.cs
@@ -159,7 +159,8 @@ namespace Spe.Client.Applications
                 var contextItemId = ContextItemId;
                 return string.IsNullOrEmpty(contextItemId)
                     ? null
-                    : Factory.GetDatabase(ContextItemDb).GetItem(new ID(contextItemId),Language.Parse(CurrentLanguage));
+                    : Factory.GetDatabase(ContextItemDb).GetItem(new ID(contextItemId),
+                        CurrentLanguage == DefaultLanguage ? Sitecore.Context.Language : Language.Parse(CurrentLanguage));
             }
         }
 