SitecorePowerShell/Console

commit 09c7b52b43e7cac6628c3bb0c95363fb616c7da4

#1302 Get-ItemReferrer / Get-ItemReference to include links from standard values item with -Recurse parameter

src/Spe/Commands/Data/GetItemReferenceCommand.cs
@@ -46,26 +46,34 @@ namespace Spe.Commands.Data
         [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
         public override string[] Language { get; set; }
 
-        [Parameter(ParameterSetName = "Item from Path, return ItemLink", Mandatory = true)]
-        [Parameter(ParameterSetName = "Item from ID, return ItemLink", Mandatory = true)]
-        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink", Mandatory = true)]
+        [Alias("Deep", "Recursive", "WithStandardValues", "WithFallback")]
+        [Parameter(ParameterSetName = "Item from Path, return Item")]
+        [Parameter(ParameterSetName = "Item from ID, return Item")]
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return Item")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
+        public SwitchParameter Recurse { get; set; }
+
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
         public SwitchParameter ItemLink { get; set; }
 
         protected override void ProcessItem(Item linkedItem)
         {
             var linkDb = Globals.LinkDatabase;
-            if (linkDb.GetReferenceCount(linkedItem) <= 0) return;
 
             if (ItemLink)
             {
                 linkDb
-                    .GetReferences(linkedItem)
+                    .GetItemReferences(linkedItem, Recurse)
                     .ToList()
                     .ForEach(WriteObject);
             }
             else
             {
-                linkDb.GetReferences(linkedItem)
+                linkDb.GetItemReferences(linkedItem, Recurse)
                     .Select(link => link.GetTargetItem())
                     .Distinct(ItemEqualityComparer.Instance)
                     .ToList()

src/Spe/Commands/Data/GetItemReferrerCommand.cs
@@ -41,26 +41,34 @@ namespace Spe.Commands.Data
         [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
         public override string[] Language { get; set; }
 
-        [Parameter(ParameterSetName = "Item from Path, return ItemLink", Mandatory = true)]
-        [Parameter(ParameterSetName = "Item from ID, return ItemLink", Mandatory = true)]
-        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink", Mandatory = true)]
+        [Alias("Deep", "Recursive", "WithStandardValues", "WithFallback")]
+        [Parameter(ParameterSetName = "Item from Path, return Item")]
+        [Parameter(ParameterSetName = "Item from ID, return Item")]
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return Item")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
+        public SwitchParameter Recurse { get; set; }
+
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
         public SwitchParameter ItemLink { get; set; }
 
         protected override void ProcessItem(Item linkedItem)
         {
             var linkDb = Globals.LinkDatabase;
-            if (linkDb.GetReferrerCount(linkedItem) <= 0) return;
 
             if (ItemLink)
             {
                 linkDb
-                    .GetReferrers(linkedItem)
+                    .GetItemReferrers(linkedItem, Recurse)
                     .ToList()
                     .ForEach(WriteObject);
             }
             else
             {
-                linkDb.GetReferrers(linkedItem)
+                linkDb.GetItemReferrers(linkedItem, Recurse)
                     .Select(link => link.GetSourceItem())
                     .Distinct(ItemEqualityComparer.Instance)
                     .ToList()

commit ccce7083cef27c5034606d6114dcc0b92b49e519

Fix for problem introduced with 09c7b52 - #1302 Get-ItemReferrer / Get-ItemReference to include links from standard values item with -Recurse parameter

src/Spe/Commands/Data/GetItemReferenceCommand.cs
@@ -55,9 +55,9 @@ namespace Spe.Commands.Data
         [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
         public SwitchParameter Recurse { get; set; }
 
-        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
-        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
-        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink", Mandatory = true)]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink", Mandatory = true)]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink", Mandatory = true)]
         public SwitchParameter ItemLink { get; set; }
 
         protected override void ProcessItem(Item linkedItem)

src/Spe/Commands/Data/GetItemReferrerCommand.cs
@@ -50,9 +50,9 @@ namespace Spe.Commands.Data
         [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
         public SwitchParameter Recurse { get; set; }
 
-        [Parameter(ParameterSetName = "Item from Path, return ItemLink")]
-        [Parameter(ParameterSetName = "Item from ID, return ItemLink")]
-        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink")]
+        [Parameter(ParameterSetName = "Item from Path, return ItemLink", Mandatory = true)]
+        [Parameter(ParameterSetName = "Item from ID, return ItemLink", Mandatory = true)]
+        [Parameter(ParameterSetName = "Item from Pipeline, return ItemLink", Mandatory = true)]
         public SwitchParameter ItemLink { get; set; }
 
         protected override void ProcessItem(Item linkedItem)