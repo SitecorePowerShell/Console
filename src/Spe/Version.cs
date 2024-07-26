8445a54500190c211ffcf7a39e807e024ea3b93c
Adam Najmanowicz <adam@najmanowicz.com>
2024-07-26 14:00:27 +0200

    8.0 version preparation

--- src/Spe/Version.cs
+++ src/Spe/Version.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("7.0.0.*")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
-[assembly: AssemblyInformationalVersion("7.0.0.0")]
+[assembly: AssemblyVersion("8.0.0.*")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyInformationalVersion("8.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sitecore PowerShell Extensions Team")]
 [assembly: AssemblyProduct("Sitecore PowerShell Extensions")]
-[assembly: AssemblyCopyright("© 2010-2023 Adam Najmanowicz, Michael West. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2010-2024 Adam Najmanowicz, Michael West. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
