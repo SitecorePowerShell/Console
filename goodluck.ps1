ba16f8f10defab1c9f65c5dad690bbf1b7b61064
Michael West <michaellwest@gmail.com>
2024-11-05 17:03:35 -0600

    Updates to build process to support docker image creation.

--- goodluck.ps1
+++ goodluck.ps1
@@ -127,3 +127,33 @@ foreach($package in $packages) {
 
     Write-Host ""
 }
+
+Write-Host "Generate docker image"
+Write-Host ""
+
+$extractedModule = Join-Path -Path $releases -ChildPath "extracted-module"
+if(Test-Path -Path $extractedModule) {
+    Remove-Item -Path $extractedModule -Recurse -Force
+}
+
+$dockerModulePath = Join-Path -Path $releases -ChildPath "docker-module"
+if(Test-Path -Path $dockerModulePath) {
+    Remove-Item -Path $dockerModulePath -Recurse -Force
+}
+
+New-Item -ItemType Directory -Path (Join-Path -Path $dockerModulePath -ChildPath "\cd\content")
+New-Item -ItemType Directory -Path (Join-Path -Path $dockerModulePath -ChildPath "\cm\content")
+New-Item -ItemType Directory -Path (Join-Path -Path $dockerModulePath -ChildPath "\db")
+New-Item -ItemType Directory -Path (Join-Path -Path $dockerModulePath -ChildPath "\solr")
+New-Item -ItemType Directory -Path (Join-Path -Path $dockerModulePath -ChildPath "\tools")
+
+$dockerModulePackage = Get-ChildItem -Path $releases -Filter "Sitecore.PowerShell.Extensions-*IAR.scwdp.zip" | Select-Object -ExpandProperty FullName
+Expand-Archive -Path $dockerModulePackage -DestinationPath $extractedModule -Force
+
+Copy-Item -Path "$($extractedModule)\Content\Website\*" -Destination (Join-Path -Path $dockerModulePath -ChildPath "\cm\content") -PassThru -Recurse
+Copy-Item -Path "$($extractedModule)\core.dacpac" -Destination (Join-Path -Path $dockerModulePath -ChildPath "\db\Sitecore.Core.dacpac") -PassThru
+
+$dockerVersion = [System.IO.Path]::GetFileName($dockermodulepackage).Replace("Sitecore.PowerShell.Extensions-", "").Replace("-IAR.scwdp.zip", "")
+
+docker build --no-cache -t "sitecorepowershell/sitecore-powershell-extensions:$($dockerVersion)-beta-1809" --build-arg BASE_IMAGE=mcr.microsoft.com/windows/nanoserver:1809 $releases
+docker build --no-cache -t "sitecorepowershell/sitecore-powershell-extensions:$($dockerVersion)-beta-ltsc2022" --build-arg BASE_IMAGE=mcr.microsoft.com/windows/nanoserver:ltsc2022 $releases
