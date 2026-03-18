b4b1d5f1310fe1dd7602b37e766e9ae91be5c1a1
Michael West <michaellwest@gmail.com>
2026-03-18 11:27:59 -0500

    Bump version to 9.0.0 with codename Powerful Ways
    - Update assembly version, file version, and informational version in Version.cs
    - Update ModuleVersion in SPE.psd1 remoting module manifest
    - Update Sitecore version manifest (Major 8 -> 9, MarketingVersion)
    - Update copyright year to 2026
    - Set codename to Powerful Ways

--- src/Spe/Version.cs
+++ src/Spe/Version.cs
@@ -2,16 +2,16 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("8.0.0.*")]
-[assembly: AssemblyFileVersion("8.0.0.0")]
-[assembly: AssemblyInformationalVersion("8.0.0.0")]
+[assembly: AssemblyVersion("9.0.0.*")]
+[assembly: AssemblyFileVersion("9.0.0.0")]
+[assembly: AssemblyInformationalVersion("9.0.0.0")]
 [assembly: AssemblyMetadata("BetaVersion", "")]
-[assembly: AssemblyMetadata("CodeName", "Wayward Son")]
+[assembly: AssemblyMetadata("CodeName", "Powerful Ways")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sitecore PowerShell Extensions Team")]
 [assembly: AssemblyProduct("Sitecore PowerShell Extensions")]
-[assembly: AssemblyCopyright("© 2010-2025 Adam Najmanowicz, Michael West. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2010-2026 Adam Najmanowicz, Michael West. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
